c410333276c8ec2da7fa327ecb0528f960d8ed68
Raimond Spekking <raymond@users.mediawiki.org>
2009-10-22 15:56:52 +0000

    * Remove weird indention (tabs are default, not spaces) * Line breaks for easier i18n at translatewiki

--- donate_interface/donate_interface.i18n.php
+++ donate_interface/donate_interface.i18n.php
@@ -8,19 +8,19 @@
 $messages = array();
  
 $messages['en'] = array( 
-	     'donate_interface' => 'Support Wikimedia',
-	     'donate-desc' => 'Donation landing page that integrates gateway extensions',
-	     'donor-intro' => 'Please choose a payment method, amount, and currency.',
-	     'donor-amount' => 'Donation amount:',
-	     'donor-other-amount' => 'Other amount',
-	     'donor-currency'  => 'Currency:',
-	     'donor-gateway' => 'Payment method:',
-	     'donor-submit-button'  => 'Donate',
-	     'donor-comment-message' => "Have a thought to share with the world? Put up to 200 characters here:",
-	     'donor-comment-label' => "Comment: ",
-	     'donor-anon-message' => "Please list my name (next to my comment) on the public donor list.", 
-	     'donor-email-agreement' => "I agree to receive future Wikimedia Foundation newsletters or emails. We will never sell or trade your information. Our donor privacy policy can be found
-<a title='Donate/Donor Privacy' href='/wiki/Donate/Donor_Privacy'>here</a>",
-       'donor-comment-title' => "Public Comment",
-	
+	'donate_interface' => 'Support Wikimedia',
+	'donate-desc' => 'Donation landing page that integrates gateway extensions',
+	'donor-intro' => 'Please choose a payment method, amount, and currency.',
+	'donor-amount' => 'Donation amount:',
+	'donor-other-amount' => 'Other amount',
+	'donor-currency'  => 'Currency:',
+	'donor-gateway' => 'Payment method:',
+	'donor-submit-button'  => 'Donate',
+	'donor-comment-message' => 'Have a thought to share with the world? Put up to 200 characters here:',
+	'donor-comment-label' => 'Comment:',
+	'donor-anon-message' => 'Please list my name (next to my comment) on the public donor list.',
+	'donor-email-agreement' => "I agree to receive future Wikimedia Foundation newsletters or emails.
+We will never sell or trade your information.
+Our donor privacy policy can be found <a title='Donate/Donor Privacy' href='/wiki/Donate/Donor_Privacy'>here</a>",
+       'donor-comment-title' => 'Public Comment',
 );
