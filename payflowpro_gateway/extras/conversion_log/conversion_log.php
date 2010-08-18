cd88d9f6750d38278f40237a509df2913a9db0a3
Arthur Richards <awjrichards@users.mediawiki.org>
2010-08-18 20:53:35 +0000

    added logging for 'rejected' transactions in conversion log

--- payflowpro_gateway/extras/conversion_log/conversion_log.php
+++ payflowpro_gateway/extras/conversion_log/conversion_log.php
@@ -16,6 +16,15 @@ class PayflowProGateway_Extras_ConversionLog extends PayflowProGateway_Extras {
 	 * Logs the response from a payflow transaction
 	 */
 	public function post_process( &$pfp_gateway_object, &$data ) {
+		// if the trxn has been outright rejected, log it
+		if ( $pfp_gateway_object->action == 'reject' ) {
+			$this->log(
+				$data[ 'contribution_tracking_id' ],
+				'Rejected'
+			);
+			return TRUE;
+		}
+
 		//make sure the payflow response property has been set (signifying a transaction has been made)
 		if ( !$pfp_gateway_object->payflow_response ) return FALSE;
 
