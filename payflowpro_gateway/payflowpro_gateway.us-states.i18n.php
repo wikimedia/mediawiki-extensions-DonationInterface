70410b589f0a03c8fa0f35215819717039e39ede
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-09-30 21:11:11 +0000

    * Follow-up r74008. Split off countries and US States into their own i18n files and register those files in $wgExtensionMessagesFiles * Revert r74036 because of the above changes so that translations can continue as before.

---
+++ payflowpro_gateway/payflowpro_gateway.us-states.i18n.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Internationalization file for the Donation Interface - PayflowPro - extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'payflowpro_gateway-state-dropdown-YY'   => 'Select a State',
+	'payflowpro_gateway-state-dropdown-XX' => 'Outside the U.S.',
+	'payflowpro_gateway-state-dropdown-AK' => 'Alaska',
+	'payflowpro_gateway-state-dropdown-AL' => 'Alabama',
+	'payflowpro_gateway-state-dropdown-AR' => 'Arkansas',
+	'payflowpro_gateway-state-dropdown-AZ' => 'Arizona',
+	'payflowpro_gateway-state-dropdown-CA' => 'California',
+	'payflowpro_gateway-state-dropdown-CO' => 'Colorado',
+	'payflowpro_gateway-state-dropdown-CT' => 'Connecticut',
+	'payflowpro_gateway-state-dropdown-DC' => 'Washington D.C.',
+	'payflowpro_gateway-state-dropdown-DE' => 'Delaware',
+	'payflowpro_gateway-state-dropdown-FL' => 'Florida',
+	'payflowpro_gateway-state-dropdown-GA' => 'Georgia',
+	'payflowpro_gateway-state-dropdown-HI' => 'Hawaii',
+	'payflowpro_gateway-state-dropdown-IA' => 'Iowa',
+	'payflowpro_gateway-state-dropdown-ID' => 'Idaho',
+	'payflowpro_gateway-state-dropdown-IL' => 'Illinois',
+	'payflowpro_gateway-state-dropdown-IN' => 'Indiana',
+	'payflowpro_gateway-state-dropdown-KS' => 'Kansas',
+	'payflowpro_gateway-state-dropdown-KY' => 'Kentucky',
+	'payflowpro_gateway-state-dropdown-LA' => 'Louisiana',
+	'payflowpro_gateway-state-dropdown-MA' => 'Massachusetts',
+	'payflowpro_gateway-state-dropdown-MD' => 'Maryland',
+	'payflowpro_gateway-state-dropdown-ME' => 'Maine',
+	'payflowpro_gateway-state-dropdown-MI' => 'Michigan',
+	'payflowpro_gateway-state-dropdown-MN' => 'Minnesota',
+	'payflowpro_gateway-state-dropdown-MO' => 'Missouri',
+	'payflowpro_gateway-state-dropdown-MS' => 'Mississippi',
+	'payflowpro_gateway-state-dropdown-MT' => 'Montana',
+	'payflowpro_gateway-state-dropdown-NC' => 'North Carolina',
+	'payflowpro_gateway-state-dropdown-ND' => 'North Dakota',
+	'payflowpro_gateway-state-dropdown-NE' => 'Nebraska',
+	'payflowpro_gateway-state-dropdown-NH' => 'New Hampshire',
+	'payflowpro_gateway-state-dropdown-NJ' => 'New Jersey',
+	'payflowpro_gateway-state-dropdown-NM' => 'New Mexico',
+	'payflowpro_gateway-state-dropdown-NV' => 'Nevada',
+	'payflowpro_gateway-state-dropdown-NY' => 'New York',
+	'payflowpro_gateway-state-dropdown-OH' => 'Ohio',
+	'payflowpro_gateway-state-dropdown-OK' => 'Oklahoma',
+	'payflowpro_gateway-state-dropdown-OR' => 'Oregon',
+	'payflowpro_gateway-state-dropdown-PA' => 'Pennsylvania',
+	'payflowpro_gateway-state-dropdown-PR' => 'Puerto Rico',
+	'payflowpro_gateway-state-dropdown-RI' => 'Rhode Island',
+	'payflowpro_gateway-state-dropdown-SC' => 'South Carolina',
+	'payflowpro_gateway-state-dropdown-SD' => 'South Dakota',
+	'payflowpro_gateway-state-dropdown-TN' => 'Tennessee',
+	'payflowpro_gateway-state-dropdown-TX' => 'Texas',
+	'payflowpro_gateway-state-dropdown-UT' => 'Utah',
+	'payflowpro_gateway-state-dropdown-VA' => 'Virginia',
+	'payflowpro_gateway-state-dropdown-VT' => 'Vermont',
+	'payflowpro_gateway-state-dropdown-WA' => 'Washington',
+	'payflowpro_gateway-state-dropdown-WI' => 'Wisconsin',
+	'payflowpro_gateway-state-dropdown-WV' => 'West Virginia',
+	'payflowpro_gateway-state-dropdown-WY' => 'Wyoming',
+	'payflowpro_gateway-state-dropdown-AA' => 'AA',
+	'payflowpro_gateway-state-dropdown-AE' => 'AE',
+	'payflowpro_gateway-state-dropdown-AP' => 'AP',
+);
