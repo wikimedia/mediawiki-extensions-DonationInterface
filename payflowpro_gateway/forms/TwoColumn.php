29b59e4dfe564a7a4ab66652d09b05d01a6e80fa
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-10-07 21:16:49 +0000

    fixing spacing

--- payflowpro_gateway/forms/TwoColumn.php
+++ payflowpro_gateway/forms/TwoColumn.php
@@ -202,7 +202,7 @@ EOT;
 		// card logos
 		$form .= '<tr>';
 		$form .= '<td />';
-		$form .= '<td>' . Xml::element( 'img', array( 'src' => $wgScriptPath . "/extensions/DonationInterface/payflowpro_gateway/includes/credit_card_logos.gif" )) . '</td>';
+		$form .= '<td>&nbsp;<br/>' . Xml::element( 'img', array( 'src' => $wgScriptPath . "/extensions/DonationInterface/payflowpro_gateway/includes/credit_card_logos.gif" )) . '</td>';
 		$form .= '</tr>';
 		
 		// card number
