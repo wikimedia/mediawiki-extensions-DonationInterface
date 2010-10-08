36a1b3dfd83d0323ff079883c78bfe6e6c6d708e
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-10-08 01:04:32 +0000

    one more fix for paypal only

--- payflowpro_gateway/forms/OneStepTwoColumn.php
+++ payflowpro_gateway/forms/OneStepTwoColumn.php
@@ -149,11 +149,13 @@ EOT;
 		$form = '';
 		$form .= Xml::openElement( 'div', array( 'id' => 'payflowpro_gateway-personal-info' ));			;
 		$form .= Xml::tags( 'h3', array( 'class' => 'payflow-cc-form-header','id' => 'payflow-cc-form-header-personal' ), wfMsg( 'payflowpro_gateway-make-your-donation' ));
-		$source = $wgRequest->getText( 'utm_source' );
-		$medium = $wgRequest->getText( 'utm_medium' );
-		$campaign = $wgRequest->getText( 'utm_campaign' );
-		$formname = $wgRequest->getText( 'form_name' );
-		$form .= Xml::Tags( 'p', array( 'id' => 'payflowpro_gateway-cc_otherways' ), wfMsg( 'payflowpro_gateway-paypal', $formname, $source, $medium, $campaign ));
+		if ( !$this->paypal ) {
+			$source = $wgRequest->getText( 'utm_source' );
+			$medium = $wgRequest->getText( 'utm_medium' );
+			$campaign = $wgRequest->getText( 'utm_campaign' );
+			$formname = $wgRequest->getText( 'form_name' );
+			$form .= Xml::Tags( 'p', array( 'id' => 'payflowpro_gateway-cc_otherways' ), wfMsg( 'payflowpro_gateway-paypal', $formname, $source, $medium, $campaign ));
+		}
 		$form .= Xml::openElement( 'table', array( 'id' => 'payflow-table-donor' ) );
 		
 		$form .= $this->generatePersonalFields();
