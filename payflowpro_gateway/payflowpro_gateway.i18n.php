b51e6bfa877644639a3f834c3e59fdc341216ce3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-10-31 14:11:53 +0000

    Move "Select a State" and "Outside the U.S." to payflowpro_gateway.i18n.php.

--- payflowpro_gateway/payflowpro_gateway.i18n.php
+++ payflowpro_gateway/payflowpro_gateway.i18n.php
@@ -116,6 +116,10 @@ In order to provide a safe, secure and pleasant experience, our donation form re
 	'payflowpro_gateway-anon-message' => 'Please list my name on the public donor list.',
 	'payflowpro_gateway-donate-wikipedia' => 'Donate on Wikipedia',
 	'payflowpro_gateway-or' => 'or'
+
+	// For more USA states, see payflowpro_gateway.us-states.i18n.php.
+	'payflowpro_gateway-state-dropdown-YY'   => 'Select a State',
+	'payflowpro_gateway-state-dropdown-XX' => 'Outside the U.S.',
 );
 
 /** Message documentation (Message documentation)
