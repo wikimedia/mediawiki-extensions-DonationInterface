c34c5fa91d8ef7419542b6580e14aedfdafbeb6b
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-11-15 21:41:07 +0000

    fixing css

--- payflowpro_gateway/forms/TwoStepTwoColumnLetter3.php
+++ payflowpro_gateway/forms/TwoStepTwoColumnLetter3.php
@@ -6,7 +6,7 @@ class PayflowProGateway_Form_TwoStepTwoColumnLetter3 extends PayflowProGateway_F
 
 		// set the path to css, before the parent constructor is called, checking to make sure some child class hasn't already set this
 		if ( !strlen( $this->getStylePath() ) ) {
-			$this->setStylePath( $wgScriptPath . '/extensions/DonationInterface/payflowpro_gateway/forms/css/TwoStepTwoColumnLetter.css' );
+			$this->setStylePath( $wgScriptPath . '/extensions/DonationInterface/payflowpro_gateway/forms/css/TwoStepTwoColumnLetter3.css' );
 		}
 
 		parent::__construct( $form_data, $form_errors );
