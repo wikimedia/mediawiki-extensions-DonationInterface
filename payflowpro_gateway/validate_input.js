7febda11d0ef74d903aee3b7df99ce722dd31308
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-11-16 01:33:00 +0000

    backing out

--- payflowpro_gateway/validate_input.js
+++ payflowpro_gateway/validate_input.js
@@ -61,12 +61,14 @@ function validate_form( form ) {
 	}
 	
 	// validate name
+	/*
 	if (document.getElementById('fname').style.color == '#999999') {
 		output += payflowproGatewayErrorMsgJs + ' ' + window['payflowproGatewayErrorMsg' + msg['Fname']] + '.\r\n';
 	}
 	if (document.getElementById('lname').style.color == '#999999') {
 		output += payflowproGatewayErrorMsgJs + ' ' + window['payflowproGatewayErrorMsg' + msg['Lname']] + '.\r\n';
 	}
+	*/
 
 	// validate email address
 	var apos = document.payment.emailAdd.value.indexOf("@");
