7e320cc36e369211435695d6b53d077806bff08d
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-12-01 23:39:41 +0000

    fix for non USD currencies

--- payflowpro_gateway/forms/TwoStepTwoColumnLetter3.php
+++ payflowpro_gateway/forms/TwoStepTwoColumnLetter3.php
@@ -222,7 +222,11 @@ EOT;
 		// card logos
 		$form .= '<tr>';
 		$form .= '<td />';
-		$form .= '<td>' . Xml::element( 'img', array( 'src' => $wgScriptPath . "/extensions/DonationInterface/payflowpro_gateway/includes/credit_card_logos.gif" ) ) . '</td>';
+		if ( $this->form_data[ 'currency' ] == 'USD' ) {
+			$form .= '<td>' . Xml::element( 'img', array( 'src' => $wgScriptPath . "/extensions/DonationInterface/payflowpro_gateway/includes/credit_card_logos.gif" ) ) . '</td>';
+		} else {
+			$form .= '<td>' . Xml::element( 'img', array( 'src' => $wgScriptPath . "/extensions/DonationInterface/payflowpro_gateway/includes/credit_card_logos3.gif" ) ) . '</td>';
+		}
 		$form .= '</tr>';
 
 		// card number
