7f968c07b6376828c2b918c3e31304f79a01117a
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-12-29 02:00:38 +0000

    size on confirmation page

--- payflowpro_gateway/forms/TwoStepTwoColumnPremium.php
+++ payflowpro_gateway/forms/TwoStepTwoColumnPremium.php
@@ -23,6 +23,7 @@ class PayflowProGateway_Form_TwoStepTwoColumnPremium extends PayflowProGateway_F
 
 		$form .= Xml::openElement( 'div', array( 'id' => 'premium-confirmation' ) );
 		$form .= Xml::element( 'img', array( 'src' => $wgScriptPath . "/extensions/DonationInterface/payflowpro_gateway/includes/Wikipedia-ten-tshirt-back.jpg", 'width' => '300', 'height' => '300' ) );
+		$form .= wfMsg( 'payflowpro_gateway-shirt-size-2', $wgRequest->getText( 'size' ) );
 		$form .= Xml::closeElement( 'div' );  // close div#premium-confirmation
 
 		$form .= Xml::closeElement( 'td' );
