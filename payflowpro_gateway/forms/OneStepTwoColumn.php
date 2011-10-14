d194f943cdbaf4566c131451b0d2bc35e3f23b39
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-10-14 18:11:26 +0000

    escaping input to prevent XSS

--- payflowpro_gateway/forms/OneStepTwoColumn.php
+++ payflowpro_gateway/forms/OneStepTwoColumn.php
@@ -215,10 +215,10 @@ EOT;
 		$form .= Xml::openElement( 'div', array( 'id' => 'payflowpro_gateway-personal-info' ) );
 		$form .= Xml::tags( 'h3', array( 'class' => 'payflow-cc-form-header', 'id' => 'payflow-cc-form-header-personal' ), wfMsg( 'payflowpro_gateway-make-your-donation' ) );
 		if ( !$this->paypal ) {
-			$source = $wgRequest->getText( 'utm_source' );
-			$medium = $wgRequest->getText( 'utm_medium' );
-			$campaign = $wgRequest->getText( 'utm_campaign' );
-			$formname = $wgRequest->getText( 'form_name' );
+			$source = htmlspecialchars( $wgRequest->getText( 'utm_source' ) );
+			$medium = htmlspecialchars( $wgRequest->getText( 'utm_medium' ) );
+			$campaign = htmlspecialchars( $wgRequest->getText( 'utm_campaign' ) );
+			$formname = htmlspecialchars( $wgRequest->getText( 'form_name' ) );
 			$form .= Xml::Tags( 'p', array( 'id' => 'payflowpro_gateway-cc_otherways' ), wfMsg( 'payflowpro_gateway-paypal', $wgScriptPath, $formname, $source, $medium, $campaign ) );
 		}
 		$form .= Xml::openElement( 'table', array( 'id' => 'payflow-table-donor' ) );
