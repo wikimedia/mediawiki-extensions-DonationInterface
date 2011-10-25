06300a7b1cb5c3e7c6cddbf353a1276d391e2c9b
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-10-25 22:14:44 +0000

    adding missing i18n file

---
+++ globalcollect_gateway/globalcollect_gateway.i18n.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Internationalization file for the Donation Interface - GlobalCollect - extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'globalcollectgateway' => 'Make your donation now',
+	'globalcollect_gateway-desc' => 'GlobalCollect payment processing',
+	'globalcollect_gateway-response-default' => 'There was an error processing your transaction.
+Please try again later.',
+);
+
+/** Message documentation (Message documentation)
+ * @author Kaldari
+ */
+$messages['qqq'] = array(
+	'globalcollectgateway' => '{{Identical|Support Wikimedia}}',
+	'globalcollect_gateway-desc' => '{{desc}}',
+	'globalcollect_gateway-response-default' => 'Error message if something went wrong on our side.',
+);
