162665b873f3f589ed8d7782fd367c6d28465ebd
Peter Gehres <pgehres@users.mediawiki.org>
2011-11-01 16:57:25 +0000

    Adding localization and template transclusion to RapidHTML

--- gateway_forms/RapidHtml.php
+++ gateway_forms/RapidHtml.php
@@ -56,6 +56,8 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 		// @captcha -> the captcha form
 		// @script_path -> maps to $wgScriptPath 
 		// @action -> generate correct form action for this form
+		// @appeal -> name of the appeal text to load
+		// @appeal_title -> name of the appeal title to load
 	);
 
 	/**
@@ -90,7 +92,7 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 		if ( $country != '' ){
 			try{
 				$country_based = $wgRequest->getText( 'ffname', 'default' ) . '-' . $country;
-		// set html-escaped filename.
+				// set html-escaped filename.
 				$this->set_html_file_path( htmlspecialchars( $country_based ));
 			} catch ( MWException $mwe ) {
 				// country-specific file does not exist, set html-escaped filename.
@@ -137,12 +139,20 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 	 * @return string The HTML form with real data in it
 	 */
 	public function add_data( $html ) {
-		global $wgScriptPath;
+		global $wgRequest, $wgOut, $wgScriptPath;
 
 		/**
 		 * This is a hack and should be replaced with something more performant.
 		 */
 		$form = $html;
+
+		// handle the appeal and appeal header
+		// TODO: determine and set variables for the default templates
+		$appeal_title_name = $this->make_safe( $wgRequest->getText( 'appeal-title', 'jimmy-appeal-title' ) );
+		$appeal_name = $this->make_safe( $wgRequest->getText( 'appeal', 'jimmy-appeal' ) );
+
+		$form = str_replace( "@appeal-title", $appeal_title_name, $form );
+		$form = str_replace( "@appeal", $appeal_name, $form );
 
 		// handle form action
 		$form = str_replace( "@action", $this->getNoCacheAction(), $form );
@@ -160,7 +170,6 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 			$form = str_replace( $token, $replace, $form );
 		}
 
-		// @fixme why do some errors have HTML in them
 		// replace errors|escape with escaped versions
 		$escape_error_tokens = array();
 		foreach ( $this->error_tokens as $token ) {
@@ -183,11 +192,43 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 
 		$form = $this->fix_dropdowns( $form );
 
-		return $form;
+		return $this->add_messages( $form );
 	}
 
 	/**
-	 * Set dropdowns to 'selected' where appropriate
+	 * Add messages into the HTML form
+	 *
+	 * @param string $html Form with tokens as placeholders for messages
+	 * @return string The HTML form containing translated messages
+	 */
+	public function add_messages( $html ) {
+		global $wgRequest, $wgOut, $wgScriptPath;
+		if( $wgRequest->getText( 'debug', 'false' ) == 'true' ){
+			# do not replace tokens
+			return $html;
+		}
+
+		# replace interface messages
+		# doing this before transclusion so that tokens can be passed as params (e.g. @language)
+		$matches = array();
+		preg_match_all( "/%([a-zA-Z0-9_-]+)%/", $html, $matches );
+		foreach( $matches[1] as $msg_key ){
+			$html = str_replace( '%' . $msg_key . '%', wfMsg( $msg_key ), $html );
+		}
+
+		# do any requested tranclusion of templates
+		$matches = array();
+		preg_match_all( "/{{((?:(?!}).)+)}}/", $html, $matches );
+		$i = 1;
+		foreach( $matches[0] as $template ){
+			# parse the template and replace in the html
+			$html = str_replace( $template, $wgOut->parse( $template ), $html );
+		}
+		return $html;
+	}
+
+	/**
+	 * Set dropdowns to "selected' where appropriate
 	 * 
 	 * This is basically a hackish fix to make sure that dropdowns stay 
 	 * 'sticky' on form submit.  This could no doubt be better.
@@ -261,7 +302,7 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 
 		// Make sure that the requested form is whitelisted
 		if ( !array_key_exists( $form_key, $allowedForms ) || ( !file_exists( $allowedForms[$form_key] )) ) {
-			throw new MWException( 'Requested an unavailable or non-existent form.' );
+			throw new MWException( 'Requested an unavailable or non-existent form.' ); # TODO: translate
 		}
 
 		$this->html_file_path = $allowedForms[ $form_key ];
@@ -275,5 +316,23 @@ class Gateway_Form_RapidHtml extends Gateway_Form {
 		if ( $wgRequest->getText( '_cache_', false )) {
 			$this->loadApiJs();
 }
+	}
+
+	/**
+	 * This function limits the possible characters passed as template keys and
+	 * values to letters, numbers, hyphens and underscores. The function also
+	 * performs standard escaping of the passed values.
+	 *
+	 * @param $string The unsafe string to escape and check for invalid characters
+	 * @return mixed|String A string matching the regex or an empty string
+	 */
+	function make_safe( $string ) {
+		$num = preg_match( '([a-zA-Z0-9_-]+)', $string, $matches );
+
+		if ( $num == 1 ){
+			# theoretically this is overkill, but better safe than sorry
+			return wfEscapeWikiText( htmlspecialchars( $matches[0] ) );
+		}
+		return '';
 	}
 }
