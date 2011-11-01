8a2fd7d3393dcc7e5f9b7a12852713fcc2a348ca
Jeremy Postlethwaite <jpostlethwaite@users.mediawiki.org>
2011-11-01 23:55:14 +0000

    Added jqueryui accordian to module gc.form.core.validate. Added css to module gc.form.core.validate.

--- donationinterface.php
+++ donationinterface.php
@@ -119,9 +119,10 @@ if ( $optionalParts['GlobalCollect'] === true ){
 	$wgResourceModules[ 'gc.form.core.validate' ] = array(
 		// scripts are not being picked up properly. These are currently only being loaded in TwoStepAmount.php
 		'scripts' => array( 'js/validate.js',  'js/jquery.validate.js', 'js/jquery.validate.additional-methods.js', ),
-		'dependencies' => array( 'ext.donationInterface.errorMessages' ),
+		'dependencies' => array( 'ext.donationInterface.errorMessages', 'jquery.ui.accordion', ),
 		'localBasePath' => $donationinterface_dir . 'globalcollect_gateway/modules',
-		'remoteExtPath' => 'DonationInterface/globalcollect_gateway/modules'
+		'remoteExtPath' => 'DonationInterface/globalcollect_gateway/modules',
+		'styles' => 'css/style.css',
 	);
 
 }
