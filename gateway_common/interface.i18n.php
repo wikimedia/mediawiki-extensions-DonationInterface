738d507a360d902cfa792d4271e62ae4b38a40f5
Jon Harald Søby <jhsoby@users.mediawiki.org>
2011-11-03 05:16:57 +0000

    Adding all known missing interface messages

--- gateway_common/interface.i18n.php
+++ gateway_common/interface.i18n.php
@@ -122,7 +122,8 @@ No processors are available.',
 	// Migrated messages
 	'donate_interface-accessible' => 'This page is only accessible from the donation page.',
 	'donate_interface-paypal-button' => 'Donate via PayPal',
-	'donate_interface-cc-button' => 'Donate by Credit Card',
+	'donate_interface-cc-button' => 'Donate by credit card',
+        'donate_interface-dd-button' => 'Donate by direct debit',
 	'donate_interface-donor-legend' => 'Donor information',
 	'donate_interface-card-legend' => 'Credit card information',
 	'donate_interface-amount-legend' => 'Donation amount',
@@ -313,7 +314,6 @@ In order to provide a safe, secure and pleasant experience, our donation form re
 	'donate_interface-on-the-back' => 'With this on the back:',
 	'donate_interface-tshirt-confirmation' => 'Your t-shirt will be shipped in the size and language below:',
 	'donate_interface-donation-tshirt' => 'Donation (t-shirt offer)',
-
 	'donate_interface-change' => 'Change',
 	'donate_interface-select-credit-card' => 'Select credit card',
 
@@ -340,9 +340,38 @@ In order to provide a safe, secure and pleasant experience, our donation form re
 	'donate_interface-translate-redlink1' => 'Help translate this page.',
 	'donate_interface-translate-redlink2' => 'Or e-mail your translation to translations@wikimedia.org',
 
+        // E-mail related variables
+        'donate_interface-email-fallbackname' => 'friend of the Wikimedia Foundation',
+        'donate_interface-email-subject' => 'Thank you from the Wikimedia Foundation',
+        'donate_interface-email-unsub-title' => 'Wikimedia Foundation unsubscribe',
+        'donate_interface-email-unsub-button' => 'Unsubscribe',
+        'donate_interface-email-unsub-success' => 'You have successfully been removed from our mailing list',
+        'donate_interface-email-unsub-delay' => 'Please allow up to four (4) days for the changes to take effect. We apologize for any emails you receive during this time. If you have any questions, please contact <donations@wikimedia.org>',
+        'donate_interface-email-unsub-fail' => 'There was an error processing your request, please contact <donations@wikimedia.org>.',
+
+        // Various form interface elements
 	'donate_interface-faqs' => 'Frequently asked questions',
 	'donate_interface-tax-info' => 'Tax deductibility information',
-	'donate_interface-informationsharing' => 'By donating, you are sharing your information with the Wikimedia Foundation, the nonprofit organization that hosts Wikipedia and other Wikimedia projects, and its service providers in the U.S. and elsewhere pursuant to our donor privacy policy. We do not sell or trade your information to anyone. For more information please read <a href="http://wikimediafoundation.org/wiki/Donor_policy/en">our donor policy</a>.'
+	'donate_interface-information_sharing' => 'By donating, you are sharing your information with the Wikimedia Foundation, the nonprofit organization that hosts Wikipedia and other Wikimedia projects, and its service providers in the U.S. and elsewhere pursuant to our donor privacy policy. We do not sell or trade your information to anyone. For more information please read <a href="http://wikimediafoundation.org/wiki/Donor_policy/en">our donor policy</a>.'
+        'donate_interface-currency-change' => 'Change?',
+        'donate_interface-bank_transfer_message' => 'Please notice that your statement will show \'Global Collect\' as the recipient of this gift. Global Collect is authorized to accept and process payments on behalf of  Wikimedia Foundation. Remember to include the reference number provided here on your bank transfer and feel free to email donations@wikimedia.org if you have any questions or concerns.',
+
+        // Tax deductibility and other legalese
+        'donate_interface-taxded-link-int' => 'Tax and other legal information',
+        'donate_interface-taxded-link-us' => 'Tax deductibility information',
+        'donate_interface-taxded-msg-int' => 'Wikimedia Foundation is a non-profit charity established in the United States under the US IRS Code Section 501(c)(3), and, for that reason, donations from persons or entities located in the United States may benefit from tax deductible status. Donations from persons or entities located outside the United States may not be eligible for tax deductions in the United States and elsewhere; in such cases, donors should seek local tax advice. Importantly, Wikimedia does not seek donations from persons or entities located in any jurisdiction that prohibits or restricts fundraising activities by international charities such as Wikimedia or applies gift taxes on donations made to such international charities.',
+        'donate_interface-taxded-msg-us' => 'Wikimedia Foundation is a non-profit charity established in the United States under the US IRS Code Section 501(c)(3), and, for that reason, donations from persons or entities located in the United States may benefit from tax deductible status.',
+        'donate_interface-legal-original' => 'This is a courtesy translation. In the event of a conflict between this translation and the English version, the English version shall govern.',
+        'donate_interface-legal-donorpolicy' => 'By donating, you are sharing your information with the Wikimedia Foundation, the nonprofit organization that hosts Wikipedia and other Wikimedia projects, and its service providers in the U.S. and elsewhere pursuant to our donor privacy policy. We do not sell or trade your information to anyone.',
+
+        // Monthly donation box
+        'donate_interface-monthlybox-title' => 'Make it monthly?',
+        'donate_interface-monthlybox-content' => 'Monthly donations are processed on the 2<sup>nd</sup> of every month. You may cancel at any time.',
+        'donate_interface-monthlybox-amount' => 'Donation amount:',
+        'donate_interface-monthlybox-yes' => 'Sure, make it monthly',
+        'donate_interface-monthlybox-no' => 'NO, make a one-time donation',
+        'donate_interface-monthlybox-bottom' => 'Your donation will be securely processed.',
+        
 	
 );
 
@@ -463,6 +492,7 @@ $messages['qqq'] = array(
 	'donate_interface-accessible' => 'Error message if you try to go to "Special:PayFlowProGateway" from anywhere else than the donation form.',
 	'donate_interface-paypal-button' => 'Button to choose to donate via PayPal.',
 	'donate_interface-cc-button' => 'Button to choose to donate by credit card.',
+        'donate_interface-dd-button' => 'Button to choose to donate by direct debit.',
 	'donate_interface-donor-legend' => 'Header for the entire form where you fill out your personal information.',
 	'donate_interface-card-legend' => 'Header for part of the form that has information about the credit card itself.',
 	'donate_interface-amount-legend' => 'Where you choose what amount to donate.',
