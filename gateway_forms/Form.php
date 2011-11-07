5a65460743b6668ca7586945b97ac493674ff38f
James Alexander <jamesur@users.mediawiki.org>
2011-11-07 00:57:21 +0000

    add secure logo function to create new secure logo and i18n messages for same

--- gateway_forms/Form.php
+++ gateway_forms/Form.php
@@ -947,4 +947,18 @@ abstract class Gateway_Form {
 		$this->payment_submethod = (string) $value;
 	}
 
+	/**
+	 * Create the Verisign logo (small size)
+	 *
+	 */
+	protected function getSmallSecureLogo($language) {
+
+	$form = '<table id="secureLogo" width="135" border="0" cellpadding="2" cellspacing="0" title=' . wfMsg('donate_interface-securelogo-title') . '>';
+	$form .= '<tr>';
+	$form .= '<td width="135" align="center" valign="top"><script type="text/javascript" src="https://seal.verisign.com/getseal?host_name=payments.wikimedia.org&amp;size=S&amp;use_flash=NO&amp;use_transparent=NO&amp;lang=en"></script><br /><a href="http://www.verisign.com/ssl-certificate/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 7px verdana,sans-serif; letter-spacing:.5px; text-align:center; margin:0px; padding:0px;">' . wfMsg('donate_interface-secureLogo-text') . '</a></td>';
+	$form .= '</tr>';
+	$form .= '</table>';
+	return $form;
+	}
 }
+
