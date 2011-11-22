b62740ddd98399363b2bc764603a8b0ad00c545b
Peter Gehres <pgehres@users.mediawiki.org>
2011-11-22 21:40:02 +0000

    Adding new GC credit card form

--- gateway_forms/rapidhtml/RapidHtmlResources.php
+++ gateway_forms/rapidhtml/RapidHtmlResources.php
@@ -131,6 +131,21 @@ $wgResourceModules[ 'gc.form.rapidhtml.webitects.dd' ] = array(
 	'localBasePath' => dirname( __FILE__ ).'/../../globalcollect_gateway/forms',
 	'remoteExtPath' => $wgGlobalCollectRapidHtmlRemoteExtPath,
 );
+$wgResourceModules[ 'gc.form.rapidhtml.cc' ] = array(
+	'styles' => 'css/gc.cc.css',
+	'scripts' => array(
+        'js/gc.cc.js',
+        'js/gc.js'
+    ),
+	'dependencies' => array( 'di.form.core.validate' ),
+	'localBasePath' => dirname( __FILE__ ).'/../../globalcollect_gateway/forms',
+	'remoteExtPath' => $wgGlobalCollectRapidHtmlRemoteExtPath,
+);
+
+
+/*************************************************************
+ *************************************************************
+ *************************************************************/
 
 // PayflowPro
 $wgResourceModules[ 'pfp.form.rapidhtml.webitects' ] = array(
