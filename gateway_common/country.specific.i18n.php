4f383b6c4249f808d69f8bd6ad8cb71e1b3e5c3b
Peter Gehres <pgehres@users.mediawiki.org>
2012-02-23 06:25:13 +0000

    Adding forms for second week of Africa testing (ET, GQ, KE, and TZ) as well as implementing new country-specific i18n file to keep the i18n files for TWN sane

---
+++ gateway_common/country.specific.i18n.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Internationalization override file for the Donation Interface extension
+ *
+ * NOTE: This file is NOT meant to be translated by TWN. Its purpose is to
+ * define message variants where the message changes between countries within
+ * the same language.
+ */
+
+// NOTE: Messages are grouped by country, then language due to the nature
+// of the use of this file
+
+$messages = array();
+
+/* CV - Cape Verde */
+$messages['en']['donate_interface-donor-fname-cv'] = 'Forename';
+$messages['en']['donate_interface-donor-lname-cv'] = 'Surname';
+
+$messages['pt']['donate_interface-donor-fname-cv'] = 'Nome';
+$messages['pt']['donate_interface-donor-lname-cv'] = 'Apelido';
+
+/* ET - Ethiopia */
+$messages['en']['donate_interface-donor-lname-et'] = 'Surname';
+
+/* GQ - Equitorial Guinea */
+$messages['en']['donate_interface-donor-fname-gq'] = 'Forename';
+$messages['en']['donate_interface-donor-lname-gq'] = 'Surname';
+
+$messages['fr']['donate_interface-donor-fname-gq'] = 'Prénom';
+$messages['fr']['donate_interface-donor-lname-gq'] = 'Nom';
+
+$messages['pt']['donate_interface-donor-fname-gq'] = 'Nome';
+$messages['pt']['donate_interface-donor-lname-gq'] = 'Apelido';
+
+/* TZ - Tanzania */
+$messages['en']['donate_interface-donor-lname-tz'] = 'Surname';
+
+$messages['sw']['donate_interface-donor-lname-tz'] = 'Ubini';
