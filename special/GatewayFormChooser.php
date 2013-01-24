21162439f2d66178e54fc0884e74690fd116973f
Peter Gehres <pgehres@wikimedia.org>
2013-01-24 11:19:51 -0800

    Making GatewayFormChooser work
    Change-Id: I400e9f40c15c4c036972f13a1c1d14bf41e9f858

--- special/GatewayFormChooser.php
+++ special/GatewayFormChooser.php
@@ -31,125 +31,40 @@ class GatewayFormChooser extends UnlistedSpecialPage {
 			echo "<pre>" . print_r( $forms, true ) . "</pre>";
 			$form = self::pickOneForm( $forms, $currency, $country );
 			echo "<pre>I choose you, " . print_r( $form, true) . "!</pre>";
+			echo "<pre>Trying: " . ucfirst($forms[$form]['gateway']) . "Gateway</pre>";
 			die();
 		}
 
-		$formSpec = $this->obtainFormSpecification( $country, $currency, $paymentMethod,
-			$paymentSubMethod );
-		
-		// Now do something with the specification
-		if ($formSpec) {
-			// TODO: Make this a little more intelligent than just choosing the first one
-			switch( $formSpec['gateways'][0] ){
-				case 'globalcollect' :
-					$gateway = "GlobalCollectGateway";
-					break;
+		$forms = self::getAllValidForms( $country, $currency, $paymentMethod, $paymentSubMethod, $recurring );
+		$form = self::pickOneForm( $forms, $currency, $country );
 
-				default :
-					$gateway = "GlobalCollectGateway"; // everything defaults here
-					break;
+		// And... construct the URL
+		$params = array(
+			'form_name' => "RapidHtml",
+			'appeal' => "JimmyQuote",
+			'ffname' => $form
+		);
+
+		// Pass any other params that are set. We do not skip ffname or form_name because
+		// we wish to retain the query string override.
+		$excludeKeys = array( 'paymentmethod', 'submethod', 'title', 'recurring' );
+		foreach ( $this->getRequest()->getValues() as $key => $value ) {
+			// Skip the required variables
+			if ( !in_array( $key, $excludeKeys ) ) {
+				$params[$key] = $value;
 			}
-
-			// TODO: Also more intelligent here
-			$formname = $formSpec['forms'][0];
-
-			// And... construct the URL
-			$params = array(
-				'form_name' => "RapidHtml",
-				'appeal' => "JimmyQuote",
-				'ffname' => $formname
-			);
-
-			// Pass any other params that are set. We do not skip ffname or form_name because
-			// we wish to retain the query string override.
-			$excludeKeys = array( 'paymentmethod', 'submethod', 'title' );
-			foreach ( $this->getRequest()->getValues() as $key => $value ) {
-				// Skip the required variables
-				if ( !in_array( $key, $excludeKeys ) ) {
-					$params[$key] = $value;
-				}
-			}
-
-			// set the default redirect
-			$redirectURL = $this->getTitleFor( $gateway )->getLocalUrl( $params );
-		} else {
-			// This is an error condition, so we return something reasonable
-			// TODO: Figure out something better to do
-
-			$redirectURL = "https://wikimediafoundation.org/wiki/Ways_to_Give";
 		}
+
+		// set the default redirect
+		$redirectURL = $this->getTitleFor( ucfirst($forms[$form]['gateway']) . "Gateway" )->getLocalUrl( $params );
+
+		// This is an error condition, so we return something reasonable
+		// TODO: Figure out something better to do
+//		$redirectURL = "https://wikimediafoundation.org/wiki/Ways_to_Give";
 
 		// Perform the redirection
 		$this->getOutput()->redirect( $redirectURL );
 	}
-
-	/**
-	 * Filters through all possible forms and returns the set that matches the arguments.
-	 *
-	 * @param      $country
-	 * @param      $currency
-	 * @param      $paymentMethod
-	 * @param null $paymentSubMethod
-	 *
-	 * @return bool or array Will be FALSE if nothing matched the filter. Otherwise array with
-	 *  keys 'gateways' and 'forms'
-	 */
-	function obtainFormSpecification( $country, $currency, $paymentMethod,
-		$paymentSubMethod = null ) {
-
-		// This map should be $[country][currency][method][] with two base keys of gateway
-		// and form. These keys may be arrays themselves.
-		global $wgDonationInterfaceFormMap;
-
-		// Make sure all the required keys exist
-		$country = $this->getKey( $country, 'default', $wgDonationInterfaceFormMap );
-
-		if ($country) {
-			$currency = $this->getKey( $currency, 'default', $wgDonationInterfaceFormMap[$country] );
-
-			if ($currency) {
-				$method = $this->getKey( $paymentMethod . "_" . $paymentSubMethod,
-					$paymentMethod, $wgDonationInterfaceFormMap[$country][$currency] );
-			}
-		}
-
-		// Determine how far we got through the filter
-		if ( $country === null || $currency === null || $method === null ) {
-			return false;
-		}
-
-		// Get the final structure
-		$formSpec = $wgDonationInterfaceFormMap[$country][$currency][$method];
-
-		// Final check to make sure we have something to return
-		if( ( count( $formSpec['gateways'] ) > 0 ) &&
-			( count( $formSpec['forms'] ) > 0 )
-		) {
-			return $formSpec;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Return one of $key, $default, or null depending on what exists in the $array.
-	 *
-	 * @param $key      The first key to look for
-	 * @param $default  The second key to look for
-	 * @param $array    The haystack to search in
-	 *
-	 * @return null or string
-	 */
-	function getKey( $key, $default, $array ) {
-		if ( array_key_exists ( $key, $array ) ) {
-			return $key;
-		} elseif ( array_key_exists ( $default, $array ) ) {
-			return $default;
-		} else {
-			return null;
-		}
-	}
-	
 	
 	/**
 	 * Gets all the valid forms that match the provided paramters. 
