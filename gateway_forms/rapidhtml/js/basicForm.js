0baee2bcd463b2598514a73b9e3841ceb9023bd6
Adam Roses Wight <awight@wikimedia.org>
2013-05-06 13:21:04 -0700

    Amazon recurring gateway
    Also,
    * Validate amazon requests, and display a form to fix amount if invalid.
    * Reuse the most basic js donation form validation.
    * Bad comment on GatewayForm::validateForm
    
    Change-Id: If2f0c441e33add6b044cce9215fd57cd8ca13703

---
+++ gateway_forms/rapidhtml/js/basicForm.js
@@ -0,0 +1,45 @@
+
+$( document ).ready( function () {
+
+	// check for RapidHtml errors and display, if any
+	var amountErrorString = "";
+	var billingErrorString = "";
+	var paymentErrorString = "";
+
+	// generate formatted errors to display
+	var temp = [];
+	for ( var e in amountErrors )
+		if ( amountErrors[e] != "" )
+			temp[temp.length] = amountErrors[e];
+	amountErrorString = temp.join( "<br />" );
+
+	temp = [];
+	for ( var f in billingErrors )
+		if ( billingErrors[f] != "" )
+			temp[temp.length] = billingErrors[f];
+	billingErrorString = temp.join( "<br />" );
+
+	temp = [];
+	for ( var g in paymentErrors )
+		if ( paymentErrors[g] != "" )
+			temp[temp.length] = paymentErrors[g];
+	paymentErrorString = temp.join( "<br />" );
+
+	// show the errors
+	if ( amountErrorString != "" ) {
+		$( "#topError" ).html( amountErrorString );
+	} else if ( billingErrorString != "" ) {
+		$( "#topError" ).html( billingErrorString );
+	} else if ( paymentErrorString != "" ) {
+		$( "#topError" ).html( paymentErrorString );
+	}
+
+	$( "#paymentContinueBtn" ).click( function() {
+		if ( validateAmount() ) {
+			document.payment.action = actionURL;
+			document.payment.submit();
+		}
+	} );
+
+} );
+
