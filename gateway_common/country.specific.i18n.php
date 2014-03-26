0e1f29e675393e9dcf6558e4984e0bbbed3d91d8
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-26 15:57:07 +0100

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ib2e017848eaf6a9d64524f6ad4ea76033212fdbc

--- gateway_common/country.specific.i18n.php
+++ gateway_common/country.specific.i18n.php
@@ -1,83 +1,20 @@
 <?php
-
-/**
- * Internationalization override file for the Donation Interface extension
- *
- * NOTE: This file is NOT meant to be translated by TWN. Its purpose is to
- * define message variants where the message changes between countries within
- * the same language.
- */
-
-// NOTE: Messages are grouped by country, then language due to the nature
-// of the use of this file
-
 $messages = array();
+$GLOBALS['wgHooks']['LocalisationCacheRecache'][] = function ( $cache, $code, &$cachedData ) {
+	$codeSequence = array_merge( array( $code ), $cachedData['fallbackSequence'] );
+	foreach ( $codeSequence as $csCode ) {
+		$fileName = __DIR__ . "/i18n/country-specific/$csCode.json";
+		if ( is_readable( $fileName ) ) {
+			$data = FormatJson::decode( file_get_contents( $fileName ), true );
+			foreach ( array_keys( $data ) as $key ) {
+				if ( $key === '' || $key[0] === '@' ) {
+					unset( $data[$key] );
+				}
+			}
+			$cachedData['messages'] = array_merge( $data, $cachedData['messages'] );
+		}
 
-/* AU - Australia */
-$messages['en']['donate_interface-city-town-au'] = 'Suburb / Town';
-
-/* CI - Cô̫te d'Ivoire */
-$messages['en']['donate_interface-donor-lname-ci'] = 'Surname';
-$messages['en']['donate_interface-donor-postal-ci'] = 'Zone';
-
-$messages['fr']['donate_interface-donor-email-ci'] = 'Adresse électronique';
-$messages['fr']['donate_interface-donor-name-ci'] = 'Prénom';
-$messages['fr']['donate_interface-donor-postal-ci'] = 'Zone';
-
-/* CM - Cameroon */
-$messages['en']['donate_interface-donor-lname-cm'] = 'Surname';
-
-$messages['fr']['donate_interface-donor-email-cm'] = 'Adresse électronique';
-$messages['fr']['donate_interface-donor-name-cm'] = 'Prénom';
-
-/* CV - Cape Verde */
-$messages['en']['donate_interface-donor-fname-cv'] = 'Forename';
-$messages['en']['donate_interface-donor-lname-cv'] = 'Surname';
-
-$messages['pt']['donate_interface-donor-fname-cv'] = 'Nome';
-$messages['pt']['donate_interface-donor-lname-cv'] = 'Apelido';
-
-/* ET - Ethiopia */
-$messages['en']['donate_interface-donor-lname-et'] = 'Surname';
-
-/* GQ - Equitorial Guinea */
-$messages['en']['donate_interface-donor-fname-gq'] = 'Forename';
-$messages['en']['donate_interface-donor-lname-gq'] = 'Surname';
-
-$messages['fr']['donate_interface-donor-fname-gq'] = 'Prénom';
-$messages['fr']['donate_interface-donor-lname-gq'] = 'Nom';
-
-$messages['pt']['donate_interface-donor-fname-gq'] = 'Nome';
-$messages['pt']['donate_interface-donor-lname-gq'] = 'Apelido';
-
-/* KE - Kenya */
-$messages['en']['donate_interface-donor-lname-ke'] = 'Surname';
-
-/* MG - Madagascar */
-$messages['en']['donate_interface-donor-lname-mg'] = 'Surname';
-$messages['en']['donate_interface-donor-postal-mg'] = 'Postcode';
-
-$messages['fr']['donate_interface-donor-email-mg'] = 'Adresse électronique';
-
-/* MR - Mauritania */
-$messages['en']['donate_interface-donor-lname-mr'] = 'Surname';
-
-$messages['fr']['donate_interface-donor-email-mr'] = 'Adresse électronique';
-$messages['fr']['donate_interface-donor-name-mr'] = 'Prénom';
-
-/* SN - Senegal */
-$messages['en']['donate_interface-donor-lname-sn'] = 'Surname';
-$messages['en']['donate_interface-donor-postal-sn'] = 'Postcode';
-
-$messages['fr']['donate_interface-donor-email-sn'] = 'Adresse électronique';
-$messages['fr']['donate_interface-donor-name-sn'] = 'Prénom';
-
-/* TZ - Tanzania */
-$messages['en']['donate_interface-donor-lname-tz'] = 'Surname';
-
-$messages['sw']['donate_interface-donor-lname-tz'] = 'Ubini';
-
-/* UG - Uganda */
-$messages['en']['donate_interface-donor-lname-ug'] = 'Surname';
-
-$messages['sw']['donate_interface-donor-lname-ug'] = 'Ubini';
+		$cachedData['deps'][] = new FileDependency( $fileName );
+	}
+	return true;
+};
