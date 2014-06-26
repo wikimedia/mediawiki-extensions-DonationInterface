fa496bc52cc265335938dac803999db8447fd9a6
Elliott Eggleston <ejegg@ejegg.com>
2014-06-25 17:52:52 -0700

    Add generic currency fallback with optional notification
    Add FallbackCurrency and NotifyOnConvert gateway globals to enable
    converting unsupported currencies to a default and optionally notfying
    the donor.
    
    Moved the conversion logic from PaypalGateway to new GatewayForm
    method fallbackToDefaultCurrency.  GatewayForm::validateForm calls
    this and notifies if conversion has occurred and either notification
    is enabled or any other form error exist.
    
    Conflicts:
    	tests/AllTests.php
    	tests/includes/test_gateway/test.adapter.php
    
    Change-Id: I6e236792a65b3fb06bb3cf6791410245a4833217

--- paypal_gateway/paypal_gateway.body.php
+++ paypal_gateway/paypal_gateway.body.php
@@ -35,48 +35,27 @@ class PaypalGateway extends GatewayForm {
 		$this->setHeaders();
 
 		if ( $this->validateForm() ) {
-			$form_errors = $this->adapter->getValidationErrors();
-			if ( array_key_exists( 'currency_code', $form_errors ) ) {
-				// If the currency is invalid, fallback to USD
-				$oldCurrency = $this->getRequest()->getText( 'currency_code' );
-				$approxConverted = 0;
-				require_once( __DIR__ . '/../gateway_common/currencyRates.inc' );
-				$conversionRates = getCurrencyRates();
-				if ( array_key_exists( $oldCurrency, $conversionRates ) ) {
-					$approxConverted = floor( $this->getRequest()->getText( 'amount' ) / $conversionRates[$oldCurrency] );
-				}
+			$this->displayForm();
+			return;
+		}
 
-				$this->adapter->addData( array(
-					'amount' => $approxConverted,
-					'currency_code' => 'USD',
-				) );
-
-				// Notify user that this has happened
-				$this->adapter->addManualError( array(
-					'general' => $this->msg( 'donate_interface-fallback-currency-notice', 'USD' )->text(),
-				) );
-
-				$this->adapter->log( "Unsupported currency forced to USD, user notified of action." );
-			}
+		// We also switch on the form name--if we're redirecting without stopping
+		// for user interaction, the form name is our only clue that this is recurring.
+		if ( $this->getRequest()->getText( 'ffname', 'default' ) === 'paypal-recurring'
+			or $this->getRequest()->getText( 'recurring', 0 )
+		) {
+			$result = $this->adapter->do_transaction( 'DonateRecurring' );
 		} else {
-			// We also switch on the form name--if we're redirecting without stopping
-			// for user interaction, the form name is our only clue that this is recurring.
-			if ( $this->getRequest()->getText( 'ffname', 'default' ) === 'paypal-recurring'
-				or $this->getRequest()->getText( 'recurring', 0 )
-			) {
-				$result = $this->adapter->do_transaction( 'DonateRecurring' );
+			$country = $this->adapter->getData_Unstaged_Escaped( 'country' );
+			if ( array_search( $country, $this->adapter->getGlobal( 'XclickCountries' ) ) !== false ) {
+				$result = $this->adapter->do_transaction( 'DonateXclick' );
 			} else {
-				$country = $this->adapter->getData_Unstaged_Escaped( 'country' );
-				if ( array_search( $country, $this->adapter->getGlobal( 'XclickCountries' ) ) !== false ) {
-					$result = $this->adapter->do_transaction( 'DonateXclick' );
-				} else {
-					$result = $this->adapter->do_transaction( 'Donate' );
-				}
+				$result = $this->adapter->do_transaction( 'Donate' );
 			}
+		}
 
-			if ( !empty( $result['redirect'] ) ) {
-				$this->getOutput()->redirect( $result['redirect'] );
-			}
+		if ( !empty( $result['redirect'] ) ) {
+			$this->getOutput()->redirect( $result['redirect'] );
 		}
 
 		$this->displayForm();
