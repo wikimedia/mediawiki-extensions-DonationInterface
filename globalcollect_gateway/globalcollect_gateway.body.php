93c8bc52cb19b1b44160e51e252f14c4a0c75be6
Kevin Israel <pleasestand@live.com>
2014-07-18 21:11:13 -0400

    Remove usage of deprecated Xml::escapeJsString
    * Also add JS encoding in a few places
    
    Change-Id: I0321d7c505148976efccb37820e39ca17c377a92

--- globalcollect_gateway/globalcollect_gateway.body.php
+++ globalcollect_gateway/globalcollect_gateway.body.php
@@ -249,9 +249,9 @@ class GlobalCollectGateway extends GatewayForm {
 
 		$queryString = '?payment_method=' . $this->adapter->getPaymentMethod() . '&payment_submethod=' . $this->adapter->getPaymentSubmethod();
 
-		$url = $this->adapter->getThankYouPage() . $queryString;
+		$encUrl = Xml::encodeJsVar( $this->adapter->getThankYouPage() . $queryString );
 
-		$link = HTML::input('MyButton', $this->msg( 'donate_interface-bt-finished')->text(), 'button', array( 'onclick' => "window.location = '$url'" ) );
+		$link = HTML::input('MyButton', $this->msg( 'donate_interface-bt-finished')->text(), 'button', array( 'onclick' => "window.location = $encUrl" ) );
 
 		$return .= Xml::tags( 'p', array( 'style' => 'text-align:center;' ), $link );
 		$return .= Xml::closeElement( 'div' );  // $id
@@ -313,9 +313,9 @@ class GlobalCollectGateway extends GatewayForm {
 
 		$queryString = '?payment_method=' . $this->adapter->getPaymentMethod() . '&payment_submethod=' . $this->adapter->getPaymentSubmethod();
 
-		$url = $this->adapter->getThankYouPage() . $queryString;
+		$encUrl = Xml::encodeJsVar( $this->adapter->getThankYouPage() . $queryString );
 
-		$link = HTML::input('MyButton', 'finished', 'button', array( 'onclick' => "window.location = '$url'" ) );
+		$link = HTML::input('MyButton', 'finished', 'button', array( 'onclick' => "window.location = $encUrl" ) );
 
 		$return .= Xml::tags( 'p', array(), $link );
 		$return .= Xml::closeElement( 'div' );  // $id
