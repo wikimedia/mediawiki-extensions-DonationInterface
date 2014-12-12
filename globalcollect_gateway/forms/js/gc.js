d9fb138dfd6cb534d973176d634d0eb6c695d72f
Sherah Smith <ssmith@wikimedia.org>
2014-12-11 16:12:24 -0800

    Prevent double clicks on gc cc forms
    Change-Id: Ica094ca2fa542cff4452ed0e13dd43a5ab9fb635

--- globalcollect_gateway/forms/js/gc.js
+++ globalcollect_gateway/forms/js/gc.js
@@ -82,9 +82,11 @@ window.displayCreditCardForm = function () {
 					mw.generatePaymentForm( data );
 				}
 			}
+			$('.cardradio').prop('disabled', false);
 		},
 		error: function ( xhr ) {
 			alert( mw.msg( 'donate_interface-error-msg-general' ) );
+			$('.cardradio').prop('disabled', false);
 		}
 	} );
 };
