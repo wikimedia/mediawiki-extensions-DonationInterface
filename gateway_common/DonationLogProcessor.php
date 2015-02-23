4085b4e6894871854565921c0373949074998972
Elliott Eggleston <ejegg@ejegg.com>
2015-02-23 11:54:18 -0800

    Add Monolog handlers and processors
    Things that log will now log to PSR\Logger\LoggerInterface instances
    obtained from DonationLoggerFactory.  The DonationLogProcessor will
    prefix messages with relevant donation information.
    
    Bug: T86266
    Change-Id: Icf7e5a07c8c95b6dc517130070cd98c552e27ae4

---
+++ gateway_common/DonationLogProcessor.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Adds a prefix from a LogPrefixProvider to log messages
+ *
+ * @author Elliott Eggleston <eeggleston@wikimedia.org>
+ */
+class DonationLogProcessor {
+
+	protected $prefixer;
+
+	public function __construct( LogPrefixProvider $prefixer ) {
+		$this->prefixer = $prefixer;
+	}
+
+	public function __invoke( $record ) {
+		try {
+			$record['message'] = $this->prefixer->getLogMessagePrefix() . $record['message'];
+		}
+		catch ( Exception $ex ) {
+			// logging shouldn't throw any exceptions
+		}
+		return $record;
+	}
+}
---
+++ gateway_common/DonationLoggerFactory.php
@@ -0,0 +1,60 @@
+<?php
+use Monolog\Logger;
+use Monolog\Handler\SyslogHandler;
+
+/**
+ * Creates loggers for DonationInterface
+ *
+ * @author Elliott Eggleston <eeggleston@wikimedia.org>
+ */
+class DonationLoggerFactory {
+	/**
+	 * For use by test harnesses to override the instance returned
+	 * @var \Psr\Log\LoggerInterface
+	 */
+	public static $overrideLogger = null;
+
+	/**
+	 * @param GatewayAdapter $gateway Get settings from this instance
+	 * @param string $suffix Append this string to the gateway identifier
+	 * @param LogPrefixProvider $prefixer Optionally use this to override
+	 *        prefixing via the gateway.
+	 * @return \Psr\Log\LoggerInterface
+	 */
+	public static function getLogger( GatewayAdapter $gateway = null, $suffix = '', LogPrefixProvider $prefixer = null ) {
+		if ( self::$overrideLogger !== null ) {
+			return self::$overrideLogger;
+		}
+		if ( $gateway === null ) {
+			$identifier = GatewayAdapter::getLogIdentifier();
+			$useSyslog = GatewayAdapter::getGlobal( 'UseSyslog' );
+			$debug = GatewayAdapter::getGlobal( 'LogDebug' );
+		} else {
+			$identifier = $gateway::getLogIdentifier();
+			$useSyslog = $gateway::getGlobal( 'UseSyslog' );
+			$debug = $gateway::getGlobal( 'LogDebug' );
+		}
+		$identifier = $identifier . $suffix;
+
+		$logger = new Logger( $identifier );
+		$logThreshold = $debug ? Logger::DEBUG : Logger::INFO;
+
+		if ( $useSyslog ) {
+			$handler = new SyslogHandler( $identifier, LOG_USER, $logThreshold );
+		} else {
+			$handler = new WmfFrameworkLogHandler( $identifier, $logThreshold );
+		}
+
+		if ( $prefixer === null ) {
+			$prefixer = $gateway;
+		}
+
+		// If either prefixer or gateway were non-null, add a processor
+		if ( $prefixer !== null ) {
+			$processor = new DonationLogProcessor( $prefixer );
+			$handler->pushProcessor( $processor );
+		}
+		$logger->pushHandler( $handler );
+		return $logger;
+	}
+}
