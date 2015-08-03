9ecbd9f7733e044336ae01b3df05fb2a2dc275a1
Adam Roses Wight <awight@wikimedia.org>
2015-08-03 14:37:42 -0700

    Revert "Debugging hack"
    Got what I came for.
    
    This reverts commit 6d047b7b30776434b450f2c3c9b7ec09bb54ab95.
    
    Change-Id: I47c111a0ea69a986028769745f0de1857d4c5e87

--- globalcollect_gateway/scripts/orphans.php
+++ globalcollect_gateway/scripts/orphans.php
@@ -78,7 +78,6 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 		}
 		$this->logger->info( 'Removed ' . $this->removed_message_count . ' messages and antimessages.' );
 
-try {
 		do {
 			//Pull a batch of CC orphans, keeping in mind that Things May Have Happened in the small slice of time since we handled the antimessages. 
 			$orphans = $this->getOrphans();
@@ -97,11 +96,6 @@ try {
 				}
 			}
 		} while ( count( $orphans ) && $this->keepGoing() );
-} catch ( Exception $ex ) {
-	$this->logger->error( $ex->getMessage() );
-	$this->logger->error( $ex->getTraceAsString() );
-	throw $ex;
-}
 
 		//TODO: Make stats squirt out all over the place.  
 		$am = 0;
