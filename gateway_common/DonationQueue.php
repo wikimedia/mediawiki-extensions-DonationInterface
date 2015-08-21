wikimedia/mediawiki-extensions-DonationInterface

commit 696861e5710901aaf086432c3c06cd1a42ab16d9

Remove legacy STOMP code
This stops mirroring the limbo queue to ActiveMQ.

TODO:
* Use message class to determine whether to normalize?
* In another commit, rename everything that says "Stomp" in reference
to a queue.
* What's up with our timestamp handling?  It turns out we were always
taking current timestamp over anything else.  Is this correct?

Bug: T99018
Change-Id: Ic835b1d895a742a75a39d986faec6c171db91613

globalcollect_gateway/scripts/orphans.php
@@ -45,10 +45,10 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 		$this->logger = DonationLoggerFactory::getLogger( $this->adapter );
 
 		//Now, actually do the processing. 
-		$this->orphan_stomp();
+		$this->process_orphans();
 	}
 
-	protected function orphan_stomp(){
+	protected function process_orphans(){
 		echo "Slaying orphans...\n";
 		$this->removed_message_count = 0;
 		$this->start_time = time();
@@ -61,27 +61,10 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 		//things we believe we just deleted.
 		$this->handled_ids = array();
 
-		//first, we need to... clean up the limbo queue. 
-
-		// TODO: Remove STOMP code.
-		//building in some redundancy here.
-		$collider_keepGoing = true;
-		$am_called_count = 0;
-		while ( $collider_keepGoing ){
-			$antimessageCount = $this->handleStompAntiMessages();
-			$am_called_count += 1;
-			if ( $antimessageCount < 10 ){
-				$collider_keepGoing = false;
-			} else {
-				sleep(2); //two seconds. 
-			}
-		}
-		$this->logger->info( 'Removed ' . $this->removed_message_count . ' messages and antimessages.' );
-
 		do {
 			//Pull a batch of CC orphans, keeping in mind that Things May Have Happened in the small slice of time since we handled the antimessages. 
 			$orphans = $this->getOrphans();
-			echo count( $orphans ) . " orphans left this batch\n";
+			echo count( $orphans ) . " orphans left in this batch\n";
 			//..do stuff.
 			foreach ( $orphans as $correlation_id => $orphan ) {
 				//process
@@ -98,15 +81,11 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 		} while ( count( $orphans ) && $this->keepGoing() );
 
 		//TODO: Make stats squirt out all over the place.  
-		$am = 0;
 		$rec = 0;
 		$err = 0;
 		$fe = 0;
 		foreach( $this->handled_ids as $id=>$whathappened ){
 			switch ( $whathappened ){
-				case 'antimessage' : 
-					$am += 1;
-					break;
 				case 'rectified' : 
 					$rec += 1;
 					break;
@@ -119,7 +98,6 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 			}
 		}
 		$final = "\nDone! Final results: \n";
-		$final .= " $am destroyed via antimessage (called $am_called_count times) \n";
 		$final .= " $rec rectified orphans \n";
 		$final .= " $err errored out \n";
 		$final .= " $fe false orphans caught \n";
@@ -159,61 +137,10 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 	    DonationQueue::instance()->delete( $correlation_id, $queue );
 	}
 
-	function addStompCorrelationIDToAckBucket( $correlation_id, $ackNow = false ){
-		static $bucket = array();
-		$count = 50; //sure. Why not?
-		if ( $correlation_id ) {
-			$bucket[$correlation_id] = "'$correlation_id'"; //avoiding duplicates.
-			$this->handled_ids[$correlation_id] = 'antimessage';
-		}
-		if ( count( $bucket ) && ( count( $bucket ) >= $count || $ackNow ) ){
-			//ack now.
-			echo 'Acking ' . count( $bucket ) . " bucket messages.\n";
-			$selector = 'JMSCorrelationID IN (' . implode( ", ", $bucket ) . ')';
-			$ackMe = stompFetchMessages( 'cc-limbo', $selector, $count * 100 ); //This is outrageously high, but I just want to be reasonably sure we get all the matches. 
-			$retrieved_count = count( $ackMe );
-			if ( $retrieved_count ){
-				stompAckMessages( $ackMe );
-				$this->removed_message_count += $retrieved_count;
-				echo "Done acking $retrieved_count messages. \n";
-			} else {
-				echo "Oh noes! No messages retrieved for $selector...\n";
-			}
-			$bucket = array();
-		}
-
-	}
-
-	// TODO: remove STOMP function
-	function handleStompAntiMessages(){
-		$selector = "antimessage = 'true' AND gateway='globalcollect'";
-		$antimessages = stompFetchMessages( 'cc-limbo', $selector, 1000 );
-		$count = 0;
-		while ( count( $antimessages ) > 10 && $this->keepGoing() ){ //if there's an antimessage, we can ack 'em all right now. 
-			echo "Colliding " . count( $antimessages ) . " antimessages\n";
-			$count += count( $antimessages );
-			foreach ( $antimessages as $message ){
-				//add the correlation ID to the ack bucket. 
-				if (array_key_exists('correlation-id', $message->headers)) {
-					$this->addStompCorrelationIDToAckBucket( $message->headers['correlation-id'] );
-
-					// mirror to new thing
-					$this->deleteMessage( $message->headers['correlation-id'], GlobalCollectAdapter::GC_CC_LIMBO_QUEUE );
-				} else {
-					echo 'The STOMP message ' . $message->headers['message-id'] . " has no correlation ID!\n";
-				}
-			}
-			$this->addStompCorrelationIDToAckBucket( false, true ); //ack all outstanding.
-			$antimessages = stompFetchMessages( 'cc-limbo', $selector, 1000 );
-		}
-		$this->addStompCorrelationIDToAckBucket( false, true ); //this just acks everything that's waiting for it.
-		$this->logger->info( "Found $count antimessages." );
-		return $count;
-	}
-
 	protected function getOrphans() {
 		// TODO: Make this configurable.
-		$time_buffer = 60*20; //20 minutes? Sure. Why not?
+		// 20 minutes: this is exactly equal to something on Globalcollect's side.
+		$time_buffer = 60*20;
 
 		$orphans = array();
 		$false_orphans = array();
@@ -266,80 +193,10 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 
 				// Round-robin the pool before we complete the loop.
 				$queue_pool->rotate();
-
-				// TODO: stop stomping
-				$this->addStompCorrelationIDToAckBucket( $correlation_id );
 			} catch ( ConnectionException $ex ) {
 				// Drop this server, for the duration of this batch.
 				$this->logger->error( "Queue server for [$current_queue] is down! Ignoring for this run..." );
 				$queue_pool->dropCurrent();
-			}
-		}
-
-		// TODO: stop stomping
-		$this->addStompCorrelationIDToAckBucket( false, true );
-
-		return $orphans;
-	}
-
-	/**
-	 * TODO: Remove this along with other STOMP code.  Use getOrphans() instead.
-	 * Returns an array of at most $batch_size decoded orphans that we don't
-	 * think we've rectified yet. 
-	 *
-	 * @return array keys are the correlation_id, and the values are the
-	 *     decoded stomp message body. 
-	 */
-	protected function getStompOrphans(){
-		$time_buffer = 60*20; //20 minutes? Sure. Why not? 
-		$selector = "payment_method = 'cc' AND gateway='globalcollect'";
-		echo "Fetching 300 Orphans\n";
-		$messages = stompFetchMessages( 'cc-limbo', $selector, 300 );
-
-		$batch_size = 300;
-		echo "Fetching {$batch_size} Orphans\n";
-
-		$orphans = array();
-		$false_orphans = array();
-		foreach ( $messages as $message ){
-			//this next block will do quite a lot of antimessage collision 
-			//when the queue is not being railed. 
-			if ( array_key_exists('antimessage', $message->headers ) ){
-				$correlation_id = $message->headers['correlation-id'];
-				$false_orphans[] = $correlation_id;
-				echo "False Orphan! $correlation_id \n";
-			} else { 
-				//legit message
-				if ( !array_key_exists( $message->headers['correlation-id'], $this->handled_ids ) ) {
-					//check the timestamp to see if it's old enough. 
-					$decoded = json_decode($message->body, true);
-					if ( array_key_exists( 'date', $decoded ) ){
-						$elapsed = $this->start_time - $decoded['date'];
-						if ( $elapsed > $time_buffer ){
-							//we got ourselves an orphan! 
-							$correlation_id = $message->headers['correlation-id'];
-							$order_id = explode('-', $correlation_id);
-							$order_id = $order_id[1];
-							$decoded['order_id'] = $order_id;
-							$decoded = unCreateQueueMessage($decoded);
-							$decoded['card_num'] = '';
-							$orphans[$correlation_id] = $decoded;
-							echo "Found an orphan! $correlation_id \n";
-						}
-					}
-				}
-			}
-		}
-
-		// TODO: Remove STOMP block.
-		foreach ( $orphans as $cid => $data ){
-			if ( in_array( $cid, $false_orphans ) ){
-				unset( $orphans[$cid] );
-				$this->addStompCorrelationIDToAckBucket( $cid );
-				$this->handled_ids[ $cid ] = 'false_orphan';
-
-				// mirror to new thing
-				$this->deleteMessage( $cid, GlobalCollectAdapter::GC_CC_LIMBO_QUEUE );
 			}
 		}
 

DonationInterface.php
@@ -418,21 +418,6 @@ $wgWorldpayGatewayAvsZipMap = array (
 	'' => 50, //No code returned. All the points.
 );
 
-$wgStompServer = "";
-
-// In this array, 'default', 'pending', and 'limbo' are required keys for those categories of
-// transactions. The value is the name of the queue. To single out a transaction type, ie:
-// credit cards, prepend 'cc-' to the base key name.
-//
-// If the resultant queue name evaluates to false, the message will not be queued on the server.
-$wgStompQueueNames = array(
-	'default' => 'test-default',    // Previously known as $wgStompQueueName
-	'pending' => 'test-pending',    // Previously known as $wgPendingStompQueueName
-	'limbo' => 'test-limbo', // Previously known as $wgLimboStompQueueName
-	'payments-antifraud' => 'payments-antifraud', //noncritical: Basically shoving the fraud log into a database.
-	'payments-init' => 'payments-init', //noncritical: same as above with the payments-initial log
-);
-
 /**
  * @global array $wgDonationInterfaceDefaultQueueServer
  *
@@ -488,7 +473,13 @@ $wgDonationInterfaceQueues = array(
 	// 	'expiry' => 3600,
 	// 	'score_key' => 'date',
 	// ),
-	// 'limbo' => ...
+	//
+	// Example of aliasing a queue
+	//
+	// 'globalcollect-cc-limbo' => array(
+	//     # Point at the main CC limbo queue.
+	//     'queue' => 'cc-limbo',
+	// ),
 
 	// Transactions still needing action before they are settled.
 	// FIXME: who reads from this queue?
@@ -730,7 +721,6 @@ $wgDonationInterfaceUtmMediumMap = array();
  */
 $wgDonationInterfaceUtmSourceMap = array();
 
-$wgDonationInterfaceEnableStomp = false;
 $wgDonationInterfaceEnableQueue = false;
 $wgDonationInterfaceEnableConversionLog = false; //this is definitely an Extra
 $wgDonationInterfaceEnableMinfraud = false; //this is definitely an Extra
@@ -780,14 +770,6 @@ $wgDonationInterfaceGatewayAdapters[] = 'PaypalAdapter';
 
 $wgSpecialPages['WorldpayGateway'] = 'WorldpayGateway';
 $wgDonationInterfaceGatewayAdapters[] = 'WorldpayAdapter';
-
-//Stomp hooks
-// FIXME: There's no point in using hooks any more, since we're switching
-// behavior inside the callbacks and not via conditional hooking.
-$wgHooks['ParserFirstCallInit'][] = 'efStompSetup';
-$wgHooks['gwStomp'][] = 'sendSTOMP';
-$wgHooks['gwPendingStomp'][] = 'sendPendingSTOMP';
-$wgHooks['gwFreeformStomp'][] = 'sendFreeformSTOMP';
 
 //Custom Filters hooks
 $wgHooks['GatewayValidate'][] = array( 'Gateway_Extras_CustomFilters::onValidate' );
@@ -999,11 +981,6 @@ require_once __DIR__ . '/DonationInterfaceFormSettings.php';
 /**
  * FUNCTIONS
  */
-
-//---Stomp functions---
-// TODO: Encapsulate in a class, or deprecate.
-require_once( __DIR__ . '/activemq_stomp/activemq_stomp.php'  );
-$wgAutoloadClasses['Stomp'] = __DIR__ . '/activemq_stomp/Stomp.php';
 
 function efDonationInterfaceUnitTests( &$files ) {
 	global $wgAutoloadClasses;

commit 5637982c502c525600858ebf7fe32de53716f21f

Convert queue message back into DI format
Queue messages use slightly different keys than in the rest of DonationInterface.
Normalize them so we can run do_transaction on the data.

This reverts a regression I introduced in 696861e5.

TODO
* Unit tests

Bug: T109527
Change-Id: I23e44114a5c8ae1d5ad9eeab4fe285de4b55bbc9

gateway_common/DonationQueue.php
@@ -240,4 +240,38 @@ class DonationQueue {
 
 		return $message;
 	}
+
+	/**
+	 * Called by the orphan rectifier to change a queue message back into a gateway
+	 * transaction array, basically undoing the mappings from buildTransactionMessage.
+	 *
+	 * TODO: This shouldn't be necessary, see https://phabricator.wikimedia.org/T109819
+	 *
+	 * @param array $transaction Queue message
+	 *
+	 * @return array message with queue keys remapped to gateway keys
+	 */
+	public static function queueMessageToNormalized( $transaction ) {
+		// For now, this function assumes that we have a complete queue message.
+
+		$rekey = array(
+			'currency' => 'currency_code',
+			'first_name' => 'fname',
+			'gross' => 'amount',
+			'last_name' => 'lname',
+			'postal_code' => 'zip',
+			'state_province' => 'state',
+			'street_address' => 'street',
+			'supplemental_address_1' => 'street_supplemental',
+		);
+
+		foreach ( $rekey as $wire => $normal ){
+			if ( isset( $transaction[$wire] ) ){
+				$transaction[$normal] = $transaction[$wire];
+				unset($transaction[$wire]);
+			};
+		}
+
+		return $transaction;
+	}
 }

globalcollect_gateway/scripts/orphans.php
@@ -179,7 +179,8 @@ class GlobalCollectOrphanRectifier extends Maintenance {
 		$this->info( "Rectifying orphan: {$data['order_id']}" );
 		$rectified = false;
 
-		$this->adapter->loadDataAndReInit( $data, $query_contribution_tracking );
+		$normalized = DonationQueue::queueMessageToNormalized( $data );
+		$this->adapter->loadDataAndReInit( $normalized, $query_contribution_tracking );
 		$results = $this->adapter->do_transaction( 'Confirm_CreditCard' );
 		$message = $results->getMessage();
 		if ( $results->getCommunicationStatus() ){