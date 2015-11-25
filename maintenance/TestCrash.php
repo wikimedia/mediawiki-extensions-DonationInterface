a812d40951a1d35ea4129bf06eebafe9a108dde0
Adam Roses Wight <awight@wikimedia.org>
2015-11-24 16:42:02 -0800

    [TEMPORARY] Instrument to tickle error streams
    Must be reverted after use.
    
    This can be run from a mediawiki-core checkout as:
    export MW_INSTALL_PATH=`pwd`
    php extensions/DonationInterface/maintenance/TestCrash.php --error
    php extensions/DonationInterface/maintenance/TestCrash.php --exception
    php extensions/DonationInterface/maintenance/TestCrash.php --fatal
    
    Bug: T115746
    Change-Id: I757d67ebf66e15c1fb5148bdd023686ee0eb270c

---
+++ maintenance/TestCrash.php
@@ -0,0 +1,38 @@
+<?php
+// XXX only deployed for a quick debuggery party
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../../..';
+}
+
+//If you get errors on this next line, set (and export) your MW_INSTALL_PATH var. 
+require_once( "$IP/maintenance/Maintenance.php" );
+
+class TestCrash extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+
+		$this->addOption( 'error', 'Yell ->error and exit' );
+		$this->addOption( 'exception', 'Crash with an exception' );
+		$this->addOption( 'fatal', 'Do something unexpected' );
+	}
+
+	public function execute() {
+		if ( $this->getOption( 'error' ) ) {
+			$this->error( 'CRASHTEST: error', true );
+        }
+		if ( $this->getOption( 'exception' ) ) {
+			throw new Exception( 'CRASHTEST: uncaught exception' );
+        }
+		if ( $this->getOption( 'fatal' ) ) {
+			$this->error( 'CRASHTEST: fatal' );
+			$everything_and_nothing = FOO::BAR();
+        }
+
+		$this->error( 'CRASHTEST: should not reach this line.' );
+	}
+}
+
+$maintClass = 'TestCrash';
+require_once RUN_MAINTENANCE_IF_MAIN;
