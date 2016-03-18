cc8190e9d11efa07729e66993d2b44a65ef6d620
Elliott Eggleston <ejegg@ejegg.com>
2016-03-18 09:43:30 -0700

    Temporary measure: placeholder fiscal number for MX
    Workaround for AstroPay API requiring fiscal number. They have
    promised a fix as soon as next week, and we should revert this
    as soon as possible.
    
    Bug: T129552
    Change-Id: I1a9bd5f0e64628d73a21f0244cc29a2fb1c3f461

--- gateway_common/FiscalNumber.php
+++ gateway_common/FiscalNumber.php
@@ -5,7 +5,16 @@
  */
 class FiscalNumber implements StagingHelper {
 	public function stage( GatewayType $adapter, $unstagedData, &$stagedData ) {
-		if ( !empty( $unstagedData['fiscal_number'] ) ) {
+		if ( empty( $unstagedData['fiscal_number'] ) ) {
+			if ( isset($unstagedData['country']) &&
+				$unstagedData['country'] === 'MX' &&
+				$adapter->getIdentifier() === 'astropay'
+			) {
+				// Not validated, but currently required by the AstroPay API
+				// TODO: Remove this when they fix it
+				$stagedData['fiscal_number'] = '111122223333444455';
+			}
+		} else {
 			$stagedData['fiscal_number'] = preg_replace( '/[^a-zA-Z0-9]/', '', $unstagedData['fiscal_number'] );
 		}
 	}
