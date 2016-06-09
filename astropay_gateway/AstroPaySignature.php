7b6cc9b9daf79bd91de54ab2d666530344903b9b
Elliott Eggleston <ejegg@ejegg.com>
2016-06-08 20:48:14 -0700

    AstroPay signature as staging helper
    Start getting rid of some abuses of getTransactionSpecificValue
    
    Bug: T137164
    Change-Id: Ic64242201c4dc2250ba5b38f0ab97dbcdddedb8d

---
+++ astropay_gateway/AstroPaySignature.php
@@ -0,0 +1,43 @@
+<?php
+
+class AstroPaySignature implements StagingHelper {
+	/*
+	 * Sign an AstroPay NewInvoice request
+	 * TODO: switch on transaction, build correct message for refund
+	 */
+	public function stage( GatewayType $adapter, $normalized, &$stagedData ) {
+		$message = self::getNewInvoiceMessage( $stagedData );
+		$stagedData['control'] = self::calculateSignature( $adapter, $message );
+	}
+
+	public static function getNewInvoiceMessage( $stagedData ) {
+		$requiredKeys = array(
+			'order_id', 'amount', 'donor_id', 'bank_code', 'fiscal_number', 'email'
+		);
+		$stagedKeys = array_keys( $stagedData );
+		if ( array_intersect( $requiredKeys, $stagedKeys ) != $requiredKeys ) {
+			return '';
+		} else {
+			return str_replace( '+', ' ',
+				$stagedData['order_id'] . 'V'
+				. $stagedData['amount'] . 'I'
+				. $stagedData['donor_id'] . '2'
+				. $stagedData['bank_code'] . '1'
+				. $stagedData['fiscal_number'] . 'H'
+				. /* bdate omitted */ 'G'
+				. $stagedData['email'] .'Y'
+				. /* zip omitted */ 'A'
+				. /* street omitted */ 'P'
+				. /* city omitted */ 'S'
+				. /* state omitted */ 'P'
+			);
+		}
+	}
+
+	public static function calculateSignature( GatewayType $adapter, $message ) {
+		$key = $adapter->getAccountConfig( 'SecretKey' );
+		return strtoupper(
+			hash_hmac( 'sha256', pack( 'A*', $message ), pack( 'A*', $key ) )
+		);
+	}
+}
