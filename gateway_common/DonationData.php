wikimedia/mediawiki-extensions-DonationInterface

commit 15608d10497b969e88b7f2f04f56857c33a504d5

Remove referrer from queue messages
It's saved in the contribution_tracking table via db connection,
nowhere else. No need for it to show up in logs or queue messages.

Bug: T110564
Change-Id: I1f1ab26653e091443f237116452ee31750be6eaf

gateway_common/DonationData.php
@@ -910,7 +910,6 @@ class DonationData implements LogPrefixProvider {
 			'utm_medium',
 			'utm_campaign',
 			'language',
-			'referrer',
 			'email',
 			'fname',
 			'lname',

gateway_common/DonationQueue.php
@@ -209,7 +209,6 @@ class DonationQueue {
 			'language' => $transaction['language'],
 			'payment_method' => $transaction['payment_method'],
 			'payment_submethod' => $transaction['payment_submethod'],
-			'referrer' => $transaction['referrer'],
 			'response' => $transaction['response'],
 			'user_ip' => $transaction['user_ip'],
 			'utm_source' => $transaction['utm_source'],

tests/Adapter/AstroPay/AstroPayTest.php
@@ -481,7 +481,6 @@ class DonationInterface_Adapter_AstroPay_AstroPayTest extends DonationInterfaceT
 		preg_match( '/Redirecting for transaction: (.*)$/', $logged[0], $matches );
 		$detailString = $matches[1];
 		$expected = array(
-			'referrer' => 'www.yourmom.com',
 			'currency_code' => 'BRL',
 			'payment_submethod' => 'test_bank',
 			'fname' => 'Nome',

tests/DonationQueueTest.php
@@ -62,7 +62,6 @@ class DonationQueueTest extends DonationInterfaceTestCase {
 			'payment_method' => 'cc',
 			'payment_submethod' => 'visa',
 			'php-message-class' => 'SmashPig\CrmLink\Messages\DonationInterfaceMessage',
-			'referrer' => 'http://localhost.net/Ref',
 			'response' => 'Gateway response something',
 			'state' => 'AK',
 			'street' => '1 Fake St.',
@@ -75,7 +74,6 @@ class DonationQueueTest extends DonationInterfaceTestCase {
 			'contribution_tracking_id' => $this->transaction['contribution_tracking_id'],
 			'utm_source' => 'testing',
 			'language' => 'en',
-			'referrer' => 'http://localhost.net/Ref',
 			'email' => 'nobody@wikimedia.org',
 			'first_name' => 'Jen',
 			'last_name' => 'Russ',

commit 10cb1cd25b2a1770a676b20ced138b0fe4af1c85

Put referrer back in session again
Regression caused by 15608d10497b969e - I forgot to check what else
was using getMessageFields.

Bug: T139753
Change-Id: Ie91519982df43ffbff9473a991ebd3a41929988a

gateway_common/DonationData.php
@@ -954,6 +954,17 @@ class DonationData implements LogPrefixProvider {
 	}
 
 	/**
+	 * Returns an array of names of fields we store in session
+	 */
+	public static function getSessionFields() {
+		$fields = self::getMessageFields();
+		$fields[] = 'order_id';
+		$fields[] = 'appeal';
+		$fields[] = 'referrer';
+		return $fields;
+	}
+
+	/**
 	 * Basically, this is a wrapper for the WebRequest wasPosted function that
 	 * won't give us notices if we weren't even a web request.
 	 * I realize this is pretty lame.

gateway_common/gateway.adapter.php
@@ -2796,13 +2796,11 @@ abstract class GatewayAdapter implements GatewayType, LogPrefixProvider {
 	public function session_addDonorData() {
 		$this->logger->info( __FUNCTION__ . ': Refreshing all donor data' );
 		$this->session_ensure();
-		$donordata = DonationData::getMessageFields();
-		$donordata[] = 'order_id';
-		$donordata[] = 'appeal';
+		$sessionFields = DonationData::getSessionFields();
 
 		$data = array();
-		foreach ( $donordata as $item ) {
-			$data[$item] = $this->getData_Unstaged_Escaped( $item );
+		foreach ( $sessionFields as $field ) {
+			$data[$field] = $this->getData_Unstaged_Escaped( $field );
 		}
 		$this->request->setSessionData( 'Donor', $data );
 	}