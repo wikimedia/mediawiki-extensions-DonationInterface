82216117b3496923465e21965deff6336151db11
Adam Roses Wight <awight@wikimedia.org>
2016-07-16 15:41:43 -0700

    Force bad encodings to approximate UTF-8
    In case the message cannot be json_encoded due to bad Unicode, we do an inexact
    mapping and may mangle strings in hopes of getting something nearly readable.
    
    Also dials down the completed donation logging to just the getStompTransaction
    fields.
    
    Bug: T140515
    Change-Id: Iff46ad776c528262f1a73320c907ca6daf6a6d34

--- tests/LoggingTest.php
+++ tests/LoggingTest.php
@@ -50,41 +50,30 @@ class DonationInterface_LoggingTest extends DonationInterfaceTestCase {
 		$init['payment_submethod'] = 'visa';
 		$init['email'] = 'innocent@manichean.com';
 		$init['ffname'] = 'cc-vmad';
-		$init['unusual_key'] = mt_rand();
 		unset( $init['order_id'] );
 
 		$expectedObject = array(
 			'amount' => 23.45,
-			'appeal' => 'JimmyQuote',
-			'attempt_id' => '2',
-			'avs_result' => 'M',
 			'city' => 'San Francisco',
 			//'contribution_tracking_id' => '1',
 			'country' => 'US',
 			'currency_code' => 'EUR',
-			'cvv_result' => 'P',
-			'effort_id' => '1',
 			'email' => 'innocent@manichean.com',
-			'ffname' => 'cc-vmad',
 			'fname' => 'Firstname',
-			'full_name' => 'Firstname Surname',
 			'gateway' => 'globalcollect',
 			'language' => 'en',
 			'lname' => 'Surname',
-			//'order_id' => 'ORDER_ID',
 			'payment_method' => 'cc',
-			'payment_product' => 1,
 			'payment_submethod' => 'visa',
 			'recurring' => '',
-			'referrer' => 'www.yourmom.com',
-			//'returnto' => 'http =>//payments.dev/index.php/Special =>GlobalCollectGatewayResult?order_id=ORDER_ID',
-			'server_ip' => '127.0.0.1',
 			'state' => 'CA',
 			'street' => '123 Fake Street',
-			'unusual_key' => ( string ) $init['unusual_key'],
 			'user_ip' => '127.0.0.1',
 			'utm_source' => '..cc',
 			'zip' => '94105',
+			'response' => 'Original Response Status (pre-SET_PAYMENT): 200',
+			'php-message-class' => 'SmashPig\CrmLink\Messages\DonationInterfaceMessage',
+			'gateway_account' => 'test',
 		);
 
 		$gateway = $this->getFreshGatewayObject( $init );
@@ -96,11 +85,76 @@ class DonationInterface_LoggingTest extends DonationInterfaceTestCase {
 			'Should log a completion message' );
 
 		$json = str_replace( GatewayAdapter::COMPLETED_PREFACE, '', $matches[0] );
-		$actualObject = json_decode( $json, true );
-		unset( $actualObject['order_id'] );
-		unset( $actualObject['returnto'] );
-		unset( $actualObject['contribution_tracking_id'] );
+		$actualObject = $this->stripRandomFields( json_decode( $json, true ) );
 		$this->assertEquals( $expectedObject, $actualObject,
 			'Completion message is as expected' );
+	}
+
+	/**
+	 * Test robustness when passed a bad Unicode string.
+	 */
+	public function testBadUnicode() {
+		$init = $this->getDonorTestData();
+		$init['payment_method'] = 'cc';
+		$init['payment_submethod'] = 'visa';
+		$init['amount'] = '23';
+		// Fake name with a bad character encoding.
+		$init['fname'] = 'Алексан�';
+		$init['lname'] = 'Гончар';
+		$init['email'] = 'innocent@manichean.com';
+		$init['ffname'] = 'cc-vmad';
+		$init['unusual_key'] = mt_rand();
+		unset( $init['order_id'] );
+
+		$expectedObject = array(
+			'amount' => 23.45,
+			'city' => 'San Francisco',
+			'country' => 'US',
+			'currency_code' => 'EUR',
+			'email' => 'innocent@manichean.com',
+			'fname' => 'Алексанï',
+			'gateway' => 'globalcollect',
+			'language' => 'en',
+			'lname' => 'Гончар',
+			'payment_method' => 'cc',
+			'payment_submethod' => 'visa',
+			'recurring' => '',
+			'state' => 'CA',
+			'street' => '123 Fake Street',
+			'user_ip' => '127.0.0.1',
+			'utm_source' => '..cc',
+			'zip' => '94105',
+			'response' => 'Original Response Status (pre-SET_PAYMENT): 200',
+			'php-message-class' => 'SmashPig\CrmLink\Messages\DonationInterfaceMessage',
+			'gateway_account' => 'test',
+		);
+
+		$gateway = $this->getFreshGatewayObject( $init );
+		$gateway->setDummyGatewayResponseCode( '200' );
+		$gateway->do_transaction( 'Confirm_CreditCard' );
+		$preface_pattern = '/' . preg_quote( GatewayAdapter::COMPLETED_PREFACE ) . '/';
+		$matches = $this->getLogMatches( LogLevel::INFO, $preface_pattern );
+		$this->assertTrue( $matches !== false,
+			'Should log a completion message' );
+
+		$json = str_replace( GatewayAdapter::COMPLETED_PREFACE, '', $matches[0] );
+		$actualObject = $this->stripRandomFields( json_decode( $json, true ) );
+
+		$this->assertEquals( $expectedObject, $actualObject,
+			'Completion message is as expected' );
+	}
+
+	protected function stripRandomFields( $data ) {
+		$toUnset = array(
+			'contribution_tracking_id',
+			'correlation-id',
+			'date',
+			'gateway_txn_id',
+			'order_id',
+		);
+		array_map( function ( $key ) use ( &$data ) {
+			unset( $data[$key] );
+		}, $toUnset );
+		return $data;
 	}
 }
