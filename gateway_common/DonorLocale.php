781a40e57ebeb9615a784526370ed881aab7fa15
Elliott Eggleston <ejegg@ejegg.com>
2017-01-31 18:07:32 -0500

    Fix paypal result routing
    Need to unstage the language if we stage it. Also, we were missing
    an alias for the result page, which makes some wikis cry.
    
    Bug: T152626
    Change-Id: Ic20ee718fb045886e188c98c8c8cb04806413aa6

--- gateway_common/DonorLocale.php
+++ gateway_common/DonorLocale.php
@@ -4,12 +4,19 @@
  * Set staged language to a locale ID with country information
  */
 class DonorLocale
-	implements StagingHelper
+	implements StagingHelper, UnstagingHelper
 {
 	public function stage( GatewayType $adapter, $normalized, &$staged_data ) {
 		if ( isset( $normalized['language'] ) && isset( $normalized['country'] ) ) {
 			$parts = explode( '_', $normalized['language'] );
 			$staged_data['language'] = "{$parts[0]}_{$normalized['country']}";
+		}
+	}
+
+	public function unstage( GatewayType $adapter, $stagedData, &$unstagedData ) {
+		if ( isset( $stagedData['language'] ) ) {
+			$parts = explode( '_', $stagedData['language'] );
+			$unstagedData['language'] = $parts[0];
 		}
 	}
 }
