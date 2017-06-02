6d1081a79e28dc40ab31a1f0f69640443d559a3f
Elliott Eggleston <ejegg@ejegg.com>
2017-06-02 14:41:31 -0700

    Add API test for Amazon
    This would have caught the SmashPig init failure
    
    Change-Id: Id75d9f1471f092cc9cb2b81147428bee1dd28b04

---
+++ tests/phpunit/Adapter/Amazon/AmazonApiTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @group Amazon
+ * @group medium
+ */
+class AmazonApiTest extends ApiTestCase {
+	public function setUp() {
+		parent::setUp();
+		TestingAmazonAdapter::$mockClient = new MockAmazonClient();
+		$this->setMwGlobals( array(
+			'wgDonationInterfaceEnableQueue' => true,
+			'wgDonationInterfaceDefaultQueueServer' => array(
+				'type' => 'TestingQueue',
+			),
+		) );
+	}
+
+	public function tearDown() {
+		TestingAmazonAdapter::$mockClient = null;
+		parent::tearDown();
+	}
+
+	public function testDoPaymentSuccess() {
+		$params = array(
+			'amount' => '1.55',
+			'currency' => 'USD',
+			'recurring' => '0',
+			'wmf_token' => 'e601502632e5e51dc2a17a0045162272+\\',
+			'orderReferenceId' => mt_rand( 0, 10000000 ),
+			'action' => 'di_amazon_bill',
+		);
+		$session = array(
+			'Donor' => array(
+				'amount' => '1.55',
+				'currency' => 'USD',
+				'recurring' => '0',
+				'contribution_tracking_id' => mt_rand( 0, 10000000 ),
+				'country' => 'US',
+			),
+			'amazonEditToken' => 'kjaskdjahsdkjsad',
+		);
+		$apiResult = $this->doApiRequest( $params, $session );
+		$redirect = $apiResult[0]['redirect'];
+		$this->assertEquals( 'https://wikimediafoundation.org/wiki/Thank_You/en?country=US', $redirect );
+		$mockClient = TestingAmazonAdapter::$mockClient;
+		$setOrderReferenceDetailsArgs = $mockClient->calls['setOrderReferenceDetails'][0];
+		$oid = $session['Donor']['contribution_tracking_id'] . '-0';
+		$this->assertEquals( $oid, $setOrderReferenceDetailsArgs['seller_order_id'], 'Did not set order id on order reference' );
+		$this->assertEquals( $params['amount'], $setOrderReferenceDetailsArgs['amount'], 'Did not set amount on order reference' );
+
+		$this->assertEquals( $params['currency'], $setOrderReferenceDetailsArgs['currency_code'], 'Did not set currency code on order reference' );
+		$message = DonationQueue::instance()->pop( 'donations' );
+		$this->assertNotNull( $message, 'Not sending a message to the donations queue' );
+		$this->assertEquals( 'S01-0391295-0674065-C095112', $message['gateway_txn_id'], 'Queue message has wrong txn ID' );
+	}
+}
