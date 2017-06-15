5890ce71247a561feb9da2c5d01687c2ca6719f0
Elliott Eggleston <ejegg@ejegg.com>
2017-06-15 14:22:14 -0400

    Send resultswitcher reloaders to TY page
    This restores some logic we used to have that kept track of processed
    orders in a key outside of the donor data.
    
    TODO: shouldn't globalcollectadapter return true for
    isReturnProcessingRequired() ?
    
    Cherry picked and sanitized of test code for deployment.
    
    Bug: T167990
    Change-Id: I9d23953e180b85d0d7ae357442760dd5c4d4619c

--- gateway_common/gateway.adapter.php
+++ gateway_common/gateway.adapter.php
@@ -1452,6 +1452,18 @@ abstract class GatewayAdapter
 	}
 
 	/**
+	 * Gateways which return true from isReturnProcessingRequired must
+	 * override this with logic to get an ID from the request which will
+	 * identify repeated attempts to process the same payment.
+	 *
+	 * @param array $requestValues
+	 * @return int|string Order id
+	 */
+	protected function getRequestProcessId( $requestValues ) {
+		return null;
+	}
+
+	/**
 	 * Process the API response obtained from the payment processor and set
 	 * properties of transaction_response.
 	 * Default implementation just says we got a response.
@@ -2945,6 +2957,7 @@ abstract class GatewayAdapter
 				'PaymentForms',
 				'numAttempt',
 				'order_status', //for post-payment activities
+				'processed_requests', //for post-payment activities
 				'sequence',
 			);
 			$preservedData = array();
