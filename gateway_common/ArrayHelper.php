6a3f773b9c5d02b34781bdb3a72800b0544c38db
Elliott Eggleston <ejegg@ejegg.com>
2017-07-10 17:23:21 -0500

    buildRequestArray handles recursion, used for name/value
    Add a new function for building array structures for API requests,
    similar to the XML builders.
    
    Change-Id: I9f0775720408a0eff685cd311295c33e327c4b01

--- gateway_common/ArrayHelper.php
+++ gateway_common/ArrayHelper.php
@@ -19,5 +19,59 @@ class ArrayHelper {
 
 		return array_combine( $keys, $values );
 	}
+
+	/**
+	 * Create an array mirroring the given structure, but with values obtained
+	 * from a callback. Like array_map, but with a few differences
+	 *  - recurses when elements are themselves arrays
+	 *  - if the callback returns '' or false, the element is skipped in the output
+	 *  - if the element is a key with value null, the output element has no key
+	 *
+	 * @param callable $callback
+	 * @param array $structure
+	 * @return array
+	 */
+	public static function buildRequestArray( $callback, $structure ) {
+		$data = array();
+		foreach ( $structure as $key => $value ) {
+			self::addArrayElement( $data, $structure, $key, $value, $callback );
+		}
+		return $data;
+	}
+
+	protected static function addArrayElement( &$targetElement, $structureElement, $key, $value, $callback ) {
+		if ( is_numeric( $key ) ) {
+			// it's just a value, not an associative array key
+			$fieldName = $value;
+			$fieldValue = $callback( $fieldName );
+			if ( self::includeElement( $fieldValue ) ) {
+				$targetElement[$fieldName] = $fieldValue;
+			}
+			return;
+		}
+		if ( is_array( $structureElement[$key] ) ) {
+			$targetElement[$key] = array();
+			foreach ( $structureElement[$key] as $subKey => $subValue ) {
+				self::addArrayElement(
+					$targetElement[$key],
+					$structureElement[$key],
+					$subKey,
+					$subValue,
+					$callback
+				);
+			}
+			// TODO: If all children are skipped, remove $targetElement[$key] ?
+		} else if ( $structureElement[$key] === null ) {
+			// HACK: needed a way to specify non-associative arrays in the output
+			$fieldValue = $callback( $key );
+			if ( self::includeElement( $fieldValue ) ) {
+				$targetElement[] = $fieldValue;
+			}
+		}
+	}
+
+	protected static function includeElement( $value ) {
+		return ( $value !== '' && $value !== false );
+	}
 }
 
