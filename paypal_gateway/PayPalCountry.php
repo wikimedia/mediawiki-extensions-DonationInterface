d9c7a9855ef77b712fefa57308edf9be69f1c3ec
Elliott Eggleston <ejegg@ejegg.com>
2017-10-12 13:57:06 -0500

    PayPal: map Netherlands Antilles to Netherlands
    Bug: T177803
    Change-Id: I4327995c01e5939778228b48ec23514ddac515cb

--- paypal_gateway/PayPalCountry.php
+++ paypal_gateway/PayPalCountry.php
@@ -2,14 +2,19 @@
 
 class PayPalCountry implements UnstagingHelper {
 
+	static $nonStandardCodes = array(
+		'C2' => 'CN', // mutant China code for merchants outside of Chine
+		'AN' => 'NL', // Netherlands Antilles is part of Netherlands since 2010
+	);
+
 	function unstage( GatewayType $adapter, $stagedData, &$unstagedData ) {
 		if ( empty( $stagedData['country'] ) ) {
 			return;
 		}
 		$country = $stagedData['country'];
-		// Handle PayPal's mutant Chine country code
-		if ( $country === 'C2' ) {
-			$country = 'CN';
+
+		if ( array_key_exists( $country, self::$nonStandardCodes ) ) {
+			$country = self::$nonStandardCodes[$country];
 		}
 		if ( CountryValidation::isValidIsoCode( $country ) ) {
 			$unstagedData['country'] = $country;
