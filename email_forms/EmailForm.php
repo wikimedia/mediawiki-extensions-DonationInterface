19b91d3e32c00a3cf3a4377228ba44e4d507a4e5
Elliott Eggleston <ejegg@ejegg.com>
2019-04-15 19:13:49 -0500

    Rebuild email forms in DonationInterface
    New forms are at Special:EmailPreferences/optin and
    Special:EmailPreferences/unsubscribe. You can get the fancy version
    of opt-in by going to Special:EmailPreferences/optin?v=wle_001.
    
    Opt-in works, but not unsubscribe yet. That's actually OK, since
    we'll have to leave the old unsubscribe up for a while to deal
    with emails that are sitting in people's inboxes. We can just use
    the new extension for opt-in till we've got the unsubscribe working,
    at which point we can set up a redirect and get rid of the old
    extension.
    
    Bug: T218171
    Change-Id: I96d8e2c64fa96b785fbf534937275e914f9d3d1e

---
+++ email_forms/EmailForm.php
@@ -0,0 +1,107 @@
+<?php
+
+class EmailForm {
+
+	/**
+	 * @var string
+	 */
+	protected $process;
+
+	const EXTENSION = '.html.mustache';
+
+	/**
+	 * @var array
+	 */
+	protected $params;
+
+	public function __construct( $process, $params = [] ) {
+		$this->process = $process;
+		$this->params = $params;
+	}
+
+	/**
+	 * Return the rendered HTML form, using template parameters from the gateway object
+	 *
+	 * @return string
+	 * @throws RuntimeException
+	 */
+	public function getForm() {
+		$options = [
+			'helpers' => [
+				'l10n' => 'EmailForm::l10n',
+			],
+			'basedir' => [ __DIR__ . '/templates' ],
+			'fileext' => self::EXTENSION,
+		];
+		$variant = !empty( $this->params['v'] ) ? '_' . $this->params['v'] : '';
+		$fileName = $this->process . $variant . self::EXTENSION;
+		$templatePath = __DIR__ . '/templates/' . $fileName;
+		if ( !file_exists( $templatePath ) && !empty( $variant ) ) {
+			// Fall back to non-variant version if the variant doesn't exist
+			$fileName = $this->process . self::EXTENSION;
+			$templatePath = __DIR__ . '/templates/' . $fileName;
+		}
+		$templateParams = $this->getTemplateParams();
+		return MustacheHelper::render( $templatePath, $templateParams, $options );
+	}
+
+	public static function l10n( $params ) {
+		$key = array_shift( $params );
+		return call_user_func_array( 'wfMessage', [ $key, $params ] )->text();
+	}
+
+	protected function getTemplateParams() {
+		global $wgFundraisingEmailUnsubscribeHelpEmail;
+
+		$map = [
+			'e' => 'email',
+			'v' => 'variant',
+			't' => 'token',
+		];
+		$templateParams = [];
+		foreach ( $map as $short => $long ) {
+			if ( isset( $this->params[$short] ) ) {
+				$templateParams[$long] = $this->params[$short];
+			}
+		}
+		$context = RequestContext::getMain();
+
+		if ( !isset( $templateParams['token'] ) ) {
+			$session = $context->getRequest()->getSession();
+			$templateParams['token'] = $session->getToken()->toString();
+			$session->persist();
+		}
+
+		$templatePath = $context->getConfig()->get( 'ScriptPath' ) .
+			'/extensions/DonationInterface/email_forms/templates';
+		$languageCode = $context->getLanguage()->getCode();
+
+		$templateParams += [
+			'uselang' => $languageCode,
+			'template_path' => $templatePath,
+			'help_email' => $wgFundraisingEmailUnsubscribeHelpEmail,
+			'action' => $context->getRequest()->getFullRequestURL(),
+			'policy_url' => $this->getPolicyUrl( $languageCode ),
+		];
+		return $templateParams;
+	}
+
+	protected function getPolicyUrl( $languageCode ) {
+		global $wgDonationInterfacePolicyURL;
+
+		// $wgDonationInterfacePolicyURL has $language and $country variables
+		// to replace. We know language but not country.
+		$policyUrl = str_replace(
+			'$country',
+			'',
+			$wgDonationInterfacePolicyURL
+		);
+
+		$policyUrl = str_replace(
+			'$language',
+			$languageCode,
+			$policyUrl
+		);
+		return $policyUrl;
+	}
+}
