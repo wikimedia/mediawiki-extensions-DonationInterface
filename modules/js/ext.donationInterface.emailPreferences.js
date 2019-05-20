42b0d99f75e8cd4ee3f0d5c7d94450aeda02d876
Elliott Eggleston <ejegg@ejegg.com>
2019-05-20 16:45:17 -0500

    Basic email validation for opt-in
    * set 'required' attribute and use type='email' for browser-native
      validation ( https://caniuse.com/#feat=input-email-tel-url )
    
    * basic JS validation before submit, sets red error hightlight
      (CSS moved to shared file from forms-only file)
    
    Bug: T218240
    Change-Id: I1771fe53955e856730f991487da4bc91d7122bd1

---
+++ modules/js/ext.donationInterface.emailPreferences.js
@@ -0,0 +1,41 @@
+( function ( $ ) {
+	$( function () {
+		var $submitButton = $( '#continue' ),
+			$emailField = $( '#email' ),
+			isEmailValid = function () {
+				var email = $emailField.val().trim(),
+					dotPos = email.lastIndexOf( '.' ),
+					atPos = email.indexOf( '@' ),
+					lastAtPos = email.lastIndexOf( '@' );
+
+				// Needs to be not empty,
+				return email !== '' &&
+					// have a '.',
+					dotPos > -1 &&
+					// have (but not start with) an '@',
+					atPos > 0 &&
+					// only have ONE '@'
+					atPos === lastAtPos &&
+					// have at least 1 char between '@' and '.',
+					dotPos > atPos + 1 &&
+					// and not end with a dot
+					dotPos < email.length - 1;
+			};
+
+		if ( $emailField.length === 1 ) {
+			$submitButton.click( function () {
+				if ( !isEmailValid() ) {
+					$emailField.addClass( 'errorHighlight' );
+					return false;
+				}
+				return true;
+			} );
+
+			$emailField.change( function () {
+				if ( isEmailValid() ) {
+					$emailField.removeClass( 'errorHighlight' );
+				}
+			} );
+		}
+	} );
+} )( jQuery );
