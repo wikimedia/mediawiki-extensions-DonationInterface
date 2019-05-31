76beb20caf2b919235f9f0883e333a03ec10330f
jgleeson <jgleeson@wikimedia.org>
2019-05-31 12:37:16 +0100

    minor update to employer3 variant explainer text.
    Bug: T219558
    Change-Id: I5c13c267b4a18050946e6076dff026a45bb334f4

--- modules/js/ext.donationInterface.employerExplainAlt.js
+++ modules/js/ext.donationInterface.employerExplainAlt.js
@@ -1,8 +1,7 @@
 ( function ( $ ) {
 	$( function () {
 		var employerRow = $( '#employer' ).closest( 'tr' ),
-			message = 'Your employer might match your gift, doubling the impact you have. <br />' +
-				'If your employer is on our list of partners, we\'ll follow up with you.';
+			message = 'Your employer might match your gift, doubling the impact you can have. If your employer is on our list of partners, we\'ll follow up with you.';
 
 		employerRow.after( '<tr><td>' + message + '</td></tr>' );
 	} );
