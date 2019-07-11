d6be4075008d4f547721ae78e72f41b4761c5d79
Elliott Eggleston <ejegg@ejegg.com>
2019-07-11 18:50:04 -0300

    Turn on Ingenico Connect ResultSwitcherTest
    And make it work.
    
    Change-Id: I0d2a0c119af8f5de37a66bf267a7eb55c04b1e22

--- tests/phpunit/Adapter/Ingenico/ResultSwitcherTest.php
+++ tests/phpunit/Adapter/Ingenico/ResultSwitcherTest.php
@@ -1,4 +1,8 @@
 <?php
+
+use SmashPig\Core\DataStores\QueueWrapper;
+use SmashPig\CrmLink\Messages\SourceFields;
+
 /**
  * @group Fundraising
  * @group DonationInterface
@@ -10,19 +14,23 @@ class DonationInterface_Adapter_Ingenico_ResultSwitcherTest extends BaseIngenico
 	 * Assuming we've popped out of the frame, does processing succeed?
 	 */
 	public function testResultSwitcherLiberatedSuccess() {
-		$this->markTestSkipped( 'ResultSwitcher not implemented' );
 		$donorTestData = $this->getDonorTestData( 'FR' );
 		$donorTestData['payment_method'] = 'cc';
 		$donorTestData['payment_submethod'] = 'visa';
 		$donorTestData['email'] = 'innocent@localhost.net';
 		$donorTestData['order_id'] = mt_rand();
+		$this->hostedPaymentStatusResponse['createdPaymentOutput']
+			['payment']
+			['paymentOutput']
+			['amountOfMoney'] = [
+				'amount' => $donorTestData['amount'] * 100,
+				'currencyCode' => $donorTestData['currency']
+			];
 		$session['Donor'] = $donorTestData;
 		// Mark the order as already popped out of the iframe
 		$session['order_status'][$donorTestData['order_id']] = 'liberated';
 		$request = [
-			'REF' => $donorTestData['order_id'],
-			'CVVRESULT' => 'M',
-			'AVSRESULT' => '0',
+			'merchantReference' => $donorTestData['order_id'],
 			'language' => 'fr', // FIXME: verifyFormOutput conflates request with other stuff
 		];
 		$assertNodes = [
@@ -35,12 +43,25 @@ class DonationInterface_Adapter_Ingenico_ResultSwitcherTest extends BaseIngenico
 				}
 			]
 		];
-
+		$this->hostedCheckoutProvider->expects( $this->once() )
+			->method( 'getHostedPaymentStatus' )
+			->willReturn( $this->hostedPaymentStatusResponse );
+		$this->hostedCheckoutProvider->expects( $this->once() )
+			->method( 'approvePayment' )
+			->willReturn( $this->approvePaymentResponse );
 		$this->verifyFormOutput( 'IngenicoGatewayResult', $request, $assertNodes, false, $session );
-		// Make sure we logged the expected cURL attempts
-		$messages = $this->getLogMatches( 'info', '/Preparing to send GET_ORDERSTATUS transaction to Global Collect/' );
-		$this->assertNotEmpty( $messages );
-		$messages = $this->getLogMatches( 'info', '/Preparing to send SET_PAYMENT transaction to Global Collect/' );
-		$this->assertNotEmpty( $messages );
+		$queueMessage = QueueWrapper::getQueue( 'donations' )->pop();
+		SourceFields::removeFromMessage( $queueMessage );
+		$expected = $donorTestData;
+		$expected['gross'] = $donorTestData['amount'];
+		unset( $expected['ffname'] );
+		unset( $expected['referrer'] );
+		unset( $expected['amount'] );
+		unset( $expected['processor_form'] );
+		unset( $expected['postal_code'] );
+		$this->assertArraySubset(
+			$expected,
+			$queueMessage
+		);
 	}
 }
