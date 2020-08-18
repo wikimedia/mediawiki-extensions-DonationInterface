5bed46827a8d11095b39dd4c585ac359b3ff7f53
Elliott Eggleston <ejegg@ejegg.com>
2020-08-18 14:08:03 -0500

    Adyen: redirect all users
    Change-Id: I6c811f221673f1c989622e27ec6430791105aaa5

--- adyen_gateway/forms/js/adyen.js
+++ adyen_gateway/forms/js/adyen.js
@@ -1,10 +1,6 @@
 ( function ( $, mw ) {
 	var di = mw.donationInterface,
-		resultFunction,
-		skinCodes = mw.config.get( 'wgAdyenGatewaySkinCodes' ),
-		ios = /iPad|iPhone|iPod/.test( navigator.userAgent ) && !window.MSStream,
-		safari = window.safari !== undefined,
-		isRTBT = false;
+		skinCodes = mw.config.get( 'wgAdyenGatewaySkinCodes' );
 
 	function redirect( result ) {
 		var $pForm, $payment = $( '#payment-form' ),
@@ -23,57 +19,10 @@
 		$pForm.submit();
 	}
 
-	function showIframe( result ) {
-		var $pForm, $payment = $( '#payment-form' );
-
-		// Empty the div; add the target iframe; then submit the request for the iframe contents
-		$payment.append(
-			$(
-				'<iframe></iframe>', {
-					style: 'display: none; width: 100%;',
-					height: 500,
-					frameborder: 0,
-					name: 'adyen-iframe',
-					id: 'adyen-iframe'
-				}
-			)
-		);
-
-		$pForm = $(
-			'<form></form>', {
-				method: 'post',
-				action: result.iframe,
-				target: 'adyen-iframe',
-				id: 'fetch-iframe-form'
-			}
-		);
-		populateHiddenFields( result.formData, $pForm );
-		$payment.append( $pForm );
-
-		$payment.find( '#fetch-iframe-form' ).submit();
-
-		// hide our continue button so that people don't get
-		// confused with two of them
-		$( '#paymentContinueBtn' ).hide();
-		// Don't let people edit name, address, or email, since we won't
-		// see any changes they make while the iframe is open.
-		di.forms.disableInput();
-		$( '#adyen-iframe' ).show( 'blind' );
-	}
-
-	// iframe is base
-	resultFunction = showIframe;
-	$( '#processor_form' ).val( skinCodes.base );
-	if ( $( '#payment_method' ).val() === 'rtbt' ) {
-		isRTBT = true;
-	}
-	if ( ios || safari || isRTBT ) {
-		resultFunction = redirect;
-		$( '#processor_form' ).val( skinCodes.redirect );
-	}
+	$( '#processor_form' ).val( skinCodes.redirect );
 
 	di.forms.submit = function () {
-		di.forms.callDonateApi( resultFunction );
+		di.forms.callDonateApi( redirect );
 	};
 
 	function populateHiddenFields( values, $form ) {
