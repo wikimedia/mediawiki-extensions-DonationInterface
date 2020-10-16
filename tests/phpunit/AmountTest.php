a5e92a29e01ba666863d332db0efd2797464f322
Elliott Eggleston <ejegg@ejegg.com>
2020-10-16 10:18:48 -0500

    This time for sure... fix nbsp in test
    Oops, I'd been using the binary escaping rather than the unicode
    escaping!
    
    Change-Id: I87e92818ea3871c8b3ef9b80028e386c21ea5187

--- tests/phpunit/AmountTest.php
+++ tests/phpunit/AmountTest.php
@@ -275,7 +275,7 @@ class AmountTest extends DonationInterfaceTestCase {
 		$amount = $validator->format( 100.59, 'USB', 'en_US' );
 		// Unknown currency code is just plunked next to amount
 		// with a non-breaking space in between.
-		$this->assertEquals( "USB\xa0100.59", $amount );
+		$this->assertEquals( "USB\u{a0}100.59", $amount );
 
 		$amount = $validator->format( 100.59, 'USD', 'en_CA' );
 		$this->assertEquals( 'US$100.59', $amount );
