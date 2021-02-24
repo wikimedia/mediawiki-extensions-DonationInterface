553bd64ba12ba7ba4ebfbd4381024d3896d3391b
Elliott Eggleston <ejegg@ejegg.com>
2021-02-23 21:29:06 -0500

    Add core es-419 support via hook
    This keeps it out of core, letting us support es-419 wherever we
    have DonationInterface installed. With this patch, we fall back to
    es.json messages when es-419.json doesn't have a translation.
    
    Bug: T199682
    Change-Id: Ide17fdfa6447014336012290207b2f734a974586

---
+++ gateway_common/messages/MessagesEs_419.php
@@ -0,0 +1,11 @@
+<?php
+/** Latin American Spanish (español de América Latina)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'es';
