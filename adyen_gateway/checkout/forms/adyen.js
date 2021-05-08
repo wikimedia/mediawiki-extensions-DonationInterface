3c9e887fc543c3c2616e21c947daba4a92155425
Elliott Eggleston <ejegg@ejegg.com>
2021-05-07 19:09:43 -0500

    Adyen Checkout: show drop-in web card fields
    Needs more CSS tweaks
    
    This is the 'Drop-In' version, but we might want to go with the
    'Component' version instead.
    
    Bug: T281516
    Change-Id: I65409367e0c12f36fd31da926dcd949ed5ec68de

--- adyen_gateway/checkout/forms/adyen.js
+++ adyen_gateway/checkout/forms/adyen.js
@@ -1,5 +1,43 @@
+/* global AdyenCheckout */
 ( function ( $, mw ) {
 	$( '.submethods' ).before(
 		'<div id="dropin-container" />'
 	);
+	function onSubmit( state, dropin ) {
+		// Submit to our server
+	}
+	function onAdditionalDetails( state, dropin ) {
+		// Handle 3D secure
+	}
+	function onError( error ) {
+		var fieldId = error.fieldType,
+			isValid = error.error === '',
+			$fieldDiv = $( 'span[data-cse="' + fieldId + '"]' )
+				.closest( '.adyen-checkout__input-wrapper' );
+		if ( isValid ) {
+			$fieldDiv.removeClass( 'errorHighlight' );
+		} else {
+			$fieldDiv.addClass( 'errorHighlight' );
+		}
+	}
+	$( function () {
+		var checkout,
+			config = mw.config.get( 'adyenConfiguration' ),
+			dropin;
+		config.onSubmit = onSubmit;
+		config.onAdditionalDetails = onAdditionalDetails;
+		config.onError = onError;
+		checkout = new AdyenCheckout( config );
+		dropin = checkout
+			.create( 'dropin' , {
+				openFirstPaymentMethod: true,
+				showPayButton: false
+			} )
+			.mount( '#dropin-container' );
+		$( '#paymentSubmit' ).show();
+		$( '#paymentSubmitBtn' ).on( 'click', function ( evt ) {
+			mw.donationInterface.validation.validate();
+			dropin.submit( evt );
+		} );
+	} );
 } )( jQuery, mediaWiki );
