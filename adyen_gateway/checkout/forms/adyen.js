ce004a03a02a64aea8b1f23741ba5f749385a521
Elliott Eggleston <ejegg@ejegg.com>
2021-05-12 14:14:17 -0500

    Adyen Checkout: map brand code to submethod
    Bug: T281528
    Change-Id: I67505ac3f70cad2005d12d29744ff4cce060cb71

--- adyen_gateway/checkout/forms/adyen.js
+++ adyen_gateway/checkout/forms/adyen.js
@@ -3,6 +3,33 @@
 	$( '.submethods' ).before(
 		'<div id="dropin-container" />'
 	);
+
+	// TODO should we do this mapping server-side
+	// using SmashPig's ReferenceData?
+	function mapAdyenSubmethod( adyenBrandCode ) {
+		switch ( adyenBrandCode ) {
+			case 'diners':
+				return 'dc';
+			case 'bijcard':
+				return 'bij';
+			case 'cartebancaire':
+				return 'cb';
+			case 'mc-debit':
+				return 'mc';
+			case 'visadankort':
+				return 'visa';
+			case 'visadebit':
+			case 'vpay':
+				return 'visa-debit';
+			case 'visabeneficial':
+				return 'visa-beneficial';
+			case 'visaelectron':
+				return 'visa-electron';
+			default:
+				return adyenBrandCode;
+		}
+	}
+
 	function onSubmit( state, dropin ) {
 		// Submit to our server
 		if ( mw.donationInterface.validation.validate() && state.isValid ) {
@@ -12,7 +39,8 @@
 				expiration: state.data.paymentMethod.encryptedExpiryMonth,
 				// HACK HACK HACK, using a totally dumb field here to not touch API yet
 				processor_form: state.data.paymentMethod.encryptedExpiryYear,
-				cvv: state.data.paymentMethod.encryptedSecurityCode
+				cvv: state.data.paymentMethod.encryptedSecurityCode,
+				payment_submethod: mapAdyenSubmethod( state.data.paymentMethod.brand )
 			} );
 		}
 	}
