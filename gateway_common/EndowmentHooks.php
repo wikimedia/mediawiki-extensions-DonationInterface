e2ee4e6e050ab7f0cfac7b4139180ae8e174f508
pcoombe <pcoombe@wikimedia.org>
2021-10-20 12:36:53 +0100

    Update Endowment "Other ways to give" link
    Update "Other ways to give" link on Endowment payment forms to new
    page: https://wikimediaendowment.org/ways-to-give/
    
    Bug: T293877
    Change-Id: I3f2e3412b08808ad0c1b5a8d63370cc53d8bff7d

--- gateway_common/EndowmentHooks.php
+++ gateway_common/EndowmentHooks.php
@@ -9,7 +9,7 @@ class EndowmentHooks {
 	public static function onAlterPaymentFormData( &$data ) {
 		if ( isset( $data['utm_medium'] ) && $data['utm_medium'] === 'endowment' ) {
 			$data['faq_url'] = 'https://donate.wikimedia.org/wiki/FAQ#What_is_the_Wikimedia_Endowment?';
-			$data['otherways_url'] = 'https://upload.wikimedia.org/wikipedia/donate/0/08/Wikimedia_Endowment_2020_Ways_to_Give.pdf';
+			$data['otherways_url'] = 'https://wikimediaendowment.org/ways-to-give/';
 		}
 	}
 
