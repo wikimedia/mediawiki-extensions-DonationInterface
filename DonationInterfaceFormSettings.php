cfad457ab3bfe8486e5546bcfed35662d03ea091
Elliott Eggleston <ejegg@ejegg.com>
2022-04-25 18:33:21 -0400

    Fix PE form settings
    Was rebased around a language modernization patch that removed the
    'whitelist'.
    
    Bug: T303207
    Change-Id: Ic166db464a84b384dde3b87849595e9167caf779

--- DonationInterfaceFormSettings.php
+++ DonationInterfaceFormSettings.php
@@ -654,7 +654,7 @@ $forms['astropay-mx'] = [
 	],
 	'selection_weight' => 110,
 ];
-$forms_whitelist['astropay-pe'] = [
+$forms['astropay-pe'] = [
 	'gateway' => 'astropay',
 	'countries' => [ '+' => 'PE' ],
 	'currencies' => [ '+' => 'PEN' ],
