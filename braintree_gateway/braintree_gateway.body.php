33e454eb18255b0caa05d729aeced65d16c74499
jgleeson <jgleeson@wikimedia.org>
2022-05-16 23:03:52 +0100

    Add Braintree Paypal SDK
    Testing notes: https://phabricator.wikimedia.org/T302949#7956422
    
    Bug: T302949
    Change-Id: I362da48781fe8567c31bdbb5eb4679664b6821f7

--- braintree_gateway/braintree_gateway.body.php
+++ braintree_gateway/braintree_gateway.body.php
@@ -9,4 +9,21 @@ class BraintreeGateway extends GatewayPage {
 
 	protected $gatewayIdentifier = BraintreeAdapter::IDENTIFIER;
 
+	public function execute( $par ) {
+		parent::execute( $par );
+		$out = $this->getOutput();
+		$clientScript = $this->adapter->getAccountConfig( 'clientScript' );
+		$paypalScript = $this->adapter->getAccountConfig( 'paypalScript' );
+		$deviceScript = $this->adapter->getAccountConfig( 'deviceScript' );
+		$out->addScript( "<script src=\"{$clientScript}\"></script>" );
+		$out->addScript( "<script src=\"{$paypalScript}\"></script>" );
+		$out->addScript( "<script src=\"{$deviceScript}\"></script>" );
+	}
+
+	public function setClientVariables( &$vars ) {
+		parent::setClientVariables( $vars );
+		$vars['clientToken'] = $this->adapter->getClientToken();
+		$vars['wgDonationInterfaceThankYouPage'] = $this->adapter->getGlobal( 'ThankYouPage' );
+	}
+
 }
