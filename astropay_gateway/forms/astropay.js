1a41b12b796686167be165df6a24025d38046c81
wfan <wfan@wikimedia.org>
2022-05-26 10:03:22 -0700

    Verbiage to state PAN field optional in India campaign
    Bug: T308332
    Change-Id: I6d13e7c7361f9e3056415cd763749cedba0f09d8

--- astropay_gateway/forms/astropay.js
+++ astropay_gateway/forms/astropay.js
@@ -3,5 +3,11 @@
 		$( '.submethods' ).after(
 			$( '<p id="redirect-explanation">' + mw.msg( 'donate_interface-redirect-explanation' ) + '</p>' )
 		);
+
+		if ( $('#country').val() === 'IN' ) {
+			$( '#fiscal_number' ).after(
+				$( '<p style="font-size: 10px">' + mw.msg( 'donate_interface-donor-fiscal_number-explain-in' ) + '</p>' )
+			);
+		}
 	});
 } )( jQuery, mediaWiki );
