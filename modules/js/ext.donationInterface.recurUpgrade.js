04b3c80d23f85d0e04f277c65caf246d2016b8b5
Elliott Eggleston <ejegg@ejegg.com>
2024-02-05 11:36:53 -0500

    Add 'other amount' to recurring upgrade
    Also move the salutation into the left column to accomodate the
    longer right column, and remove an extraneous closing span.
    
    Focusing in the field should select the 'other' option. Clicking
    a different amount should clear the 'other' field.
    
    Bug: T356478
    Change-Id: Id911c66de3dff6f470dfae57ae9d5008c7affbd3

--- modules/js/ext.donationInterface.recurUpgrade.js
+++ modules/js/ext.donationInterface.recurUpgrade.js
@@ -1,17 +1,31 @@
 ( function ( $ ) {
 	$( function () {
 		var $submitButton = $( '#save' ),
-			$amountField = $( 'input[name=upgrade_amount]' );
+			$amountField = $( 'input[name=upgrade_amount]' ),
+			$otherAmountField = $( 'input[name=upgrade_amount_other]' );
+		function setSubmitState() {
+			var valueIsValid = false, value = $amountField.filter( ':checked' ).val();
+			if ( value === 'other' ) {
+				valueIsValid = ( $otherAmountField.val() > 0 );
+			} else if ( value > 0 ) {
+				valueIsValid = true;
+				$otherAmountField.val( '' );
+			}
+			if ( valueIsValid ) {
+				$submitButton.removeClass( 'disabled' );
+				$submitButton.prop( 'disabled', false );
+			} else {
+				$submitButton.addClass( 'disabled' );
+				$submitButton.prop( 'disabled', true );
+			}
+		}
 
-		$amountField.change( function () {
-            var value = $amountField.filter( ':checked' ).val();
-            if ( value ) {
-                $submitButton.removeClass( 'disabled' );
-                $submitButton.prop( 'disabled', false );
-            } else {
-                $submitButton.addClass( 'disabled' );
-                $submitButton.prop( 'disabled', true );
-            }
-        } );
+		$amountField.change( setSubmitState );
+		$otherAmountField.change( setSubmitState );
+		$otherAmountField.focus( function () {
+			$amountField.filter( '[value=other]' ).prop( 'checked', true );
+			setSubmitState();
+		} );
+		setSubmitState();
 	} );
 } )( jQuery );
