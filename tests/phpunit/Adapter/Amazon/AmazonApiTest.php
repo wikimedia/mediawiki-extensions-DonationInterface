6e1713b6c38a9cce993224210df3fd2ea0b8d9b1
Elliott Eggleston <ejegg@ejegg.com>
2024-03-28 11:58:17 -0400

    Use wmf_ params for redirect to TY pages (DEPLOY NOTE)
    Do not merge / deploy till Peter Coombe has updated the Thank You
    page Javascript to handle the new prefix.
    
    Bug: T351325
    Change-Id: Id6bf7dfdb6ec50ba41a125eedafce914ee0082dc

--- tests/phpunit/Adapter/Amazon/AmazonApiTest.php
+++ tests/phpunit/Adapter/Amazon/AmazonApiTest.php
@@ -40,7 +40,7 @@ class AmazonApiTest extends DonationInterfaceApiTestCase {
 		];
 		$apiResult = $this->doApiRequest( $params, $session );
 		$redirect = $apiResult[0]['redirect'];
-		$this->assertEquals( 'https://donate.wikimedia.org/wiki/Thank_You/en?country=US&amount=1.55&currency=USD&payment_method=amazon&order_id=7654321-1&recurring=&utm_source=..', $redirect );
+		$this->assertEquals( 'https://donate.wikimedia.org/wiki/Thank_You/en?country=US&amount=1.55&currency=USD&payment_method=amazon&order_id=7654321-1&recurring=&wmf_source=..', $redirect );
 		$mockClient = $this->providerConfig->object( 'payments-client' );
 		$setOrderReferenceDetailsArgs = $mockClient->calls['setOrderReferenceDetails'][0];
 		$oid = $session['Donor']['contribution_tracking_id'] . '-1';
