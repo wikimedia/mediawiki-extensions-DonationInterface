657318b5d56fda5eba1373e9abe2477bf0894c43
cstone <cstone@wikimedia.org>
2024-08-06 02:35:55 +0000

    Add monthly convert variant 15
    Changes text at top, flips buttons and stacks them
    
    Bug:T371523
    Change-Id: Id377538d24c9b0b2af036f530f8daee63b906435

---
+++ modules/js/ext.donationInterface.monthlyConvert_015.js
@@ -0,0 +1,5 @@
+( function ( $ ) {
+	$( function () {
+		$( '.mc-choice h1' ).prepend( '<svg class="frb-icon frb-icon-heart" aria-hidden="true" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 66"><path fill="#900" d="M66 20.8C66 9.3 57.6 0 47.3 0 41.6 0 36.4 2.9 33 7.4 29.6 2.9 24.4 0 18.7 0 8.4 0 0 9.3 0 20.8 0 27 2.5 32.5 6.3 36.3L33.1 66l28-31.2c3-3.7 4.9-8.6 4.9-14z"></path></svg>&nbsp;' );
+	} );
+} )( jQuery );
