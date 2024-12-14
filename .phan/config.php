c584c0cec1bd8cda181b69152ab764149040269d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-12-14 01:08:47 +0000

    build: Updating mediawiki/mediawiki-phan-config to 0.15.0
    Change-Id: Ie0962ba8d47d399c8a0fa3f82b8f262ec1722f9b

--- .phan/config.php
+++ .phan/config.php
@@ -4,6 +4,9 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 $cfg['target_php_version'] = '7.4';
 
+// Ignored to allow upgrading Phan, to be fixed later.
+$cfg['suppress_issue_types'][] = 'MediaWikiNoIssetIfDefined';
+
 $cfg['file_list'] = array_merge(
 	$cfg['file_list'],
 	[
