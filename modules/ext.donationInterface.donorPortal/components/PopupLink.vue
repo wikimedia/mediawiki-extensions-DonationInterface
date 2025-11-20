55e70189d0ede135398dbfb3a4112b5a6572fbd2
wfan <wfan@wikimedia.org>
2025-11-20 09:22:49 -0800

    Donor Portal: Add popup component
    Use slot to pass child elements
    
    Bug: T396271
    Change-Id: I37d142923a16322d5c84011940cac2447d53a92b

---
+++ modules/ext.donationInterface.donorPortal/components/PopupLink.vue
@@ -0,0 +1,47 @@
+<template>
+	<!-- Clickable Link -->
+	<a
+		href="#"
+		class="link"
+		@click.prevent="openModal">
+		<slot name="link-text"></slot>
+	</a>
+
+	<!-- Popup Modal -->
+	<div
+		v-if="isModalOpen"
+		class="popup-overlay"
+		@click="closeModal">
+		<div class="popup-modal">
+			<!-- Render HTML content -->
+			<slot name="popup-body"></slot>
+			<div class="popup-footer">
+				<button class="popup-close" @click="closeModal">
+					{{ $i18n( 'donorportal-close' ).text() }}
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+const { defineComponent, ref } = require( 'vue' );
+
+module.exports = exports = defineComponent( {
+	name: 'PopupLink',
+	setup() {
+		const isModalOpen = ref( false );
+		return {
+			isModalOpen
+		};
+	},
+	methods: {
+		openModal() {
+			this.isModalOpen = true;
+		},
+		closeModal() {
+			this.isModalOpen = false;
+		}
+	}
+} );
+</script>
