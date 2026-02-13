af79e302f38e8040ab475906020b032db1f24583
Elliott Eggleston <ejegg@ejegg.com>
2026-02-13 16:42:03 -0500

    Log donor portal navigation with beacons
    Adding 'beforeEnter: logNavigation' to any route definition will now
    cause a beacon to be sent when navigating there, including the contact
    ID, the full route, and any wmf_ parameters from the querystring.
    
    This patch adds logging to the pause, cancel, update, annual convert,
    and amount downgrade routes.
    
    Bug: T404243
    Change-Id: Ie5497e37cbd00cb944dcd44d9b5b3dfc088b4ae6

--- modules/ext.donationInterface.donorPortal/router.js
+++ modules/ext.donationInterface.donorPortal/router.js
@@ -5,16 +5,29 @@ const { createWebHashHistory, createRouter } = require( 'vue-router' ),
 	CancelDonations = require( './views/CancelDonations.vue' ),
 	UpdateDonations = require( './views/UpdateDonations.vue' ),
 	AnnualConversion = require( './views/AnnualConversion.vue' ),
-	AmountDowngrade = require( './views/AmountDowngrade.vue' );
+	AmountDowngrade = require( './views/AmountDowngrade.vue' ),
+	wmfParams = new URLSearchParams( window.location.search );
+
+for ( const k of wmfParams.keys() ) {
+	if ( !k.startsWith( 'wmf_' ) ) {
+		wmfParams.delete( k );
+	}
+}
+const wmfSuffix = ( wmfParams.size === 0 ? '' : '?' + wmfParams.toString() );
+
+function logNavigation( to ) {
+	const donorData = mw.config.get( 'donorData' );
+	navigator.sendBeacon( '/beacon/donor_portal/' + donorData.contact_id + to.fullPath + wmfSuffix );
+}
 
 const routes = [
   { path: '/', component: Home, name: 'Home' },
   { path: '/login', component: Login, name: 'Login' },
-  { path: '/pause-donations/:id', component: PauseDonations, name: 'PauseDonations' },
-  { path: '/cancel-donations/:id', component: CancelDonations, name: 'CancelDonations' },
-  { path: '/update-donations/:id', component: UpdateDonations, name: 'UpdateDonations' },
-  { path: '/annual-conversion/:id', component: AnnualConversion, name: 'AnnualConversion' },
-  { path: '/amount-downgrade/:id', component: AmountDowngrade, name: 'AmountDowngrade' },
+  { path: '/pause-donations/:id', component: PauseDonations, name: 'PauseDonations', beforeEnter: logNavigation },
+  { path: '/cancel-donations/:id', component: CancelDonations, name: 'CancelDonations', beforeEnter: logNavigation },
+  { path: '/update-donations/:id', component: UpdateDonations, name: 'UpdateDonations', beforeEnter: logNavigation },
+  { path: '/annual-conversion/:id', component: AnnualConversion, name: 'AnnualConversion', beforeEnter: logNavigation },
+  { path: '/amount-downgrade/:id', component: AmountDowngrade, name: 'AmountDowngrade', beforeEnter: logNavigation },
   { path: '/annual-conversion/:id/save', component: AnnualConversion, name: 'AnnualConversionSave' },
   { path: '/amount-downgrade/:id/save', component: AmountDowngrade, name: 'AmountDowngradeSave' }
 ];
