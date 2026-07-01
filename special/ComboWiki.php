248e271e041964f6fbda4160987fe285d17a3c3f
Jack Gleeson <jgleeson@wikimedia.org>
2026-07-01 15:45:08 +0100

    Wire GatewayRouter into ComboWiki
    Pull the routing parameters (country, currency, payment method,
    payment submethod, recurring, variant) from the request in ComboWiki
    the same way GatewayChooser does, and use GatewayRouter's
    getSupportedGateways() and chooseGatewayByPriority() to resolve which
    gateway should handle the donation, exposing the result to the Vue
    app via setClientVariables().
    
    Add ComboWikiTest covering gateway selection for credit card, PayPal,
    and Venmo payment methods.
    
    Test with:
    https://localhost:9001/index.php?title=Special:ComboWiki&payment_method=cc&recurring=0&language=en&currency=USD&amount=35&country=US&debug=true
    
    Which should select 'adyen' as the chosen gateway. To confirm this, open the browser console and check the contents of:
    mw.config.get( 'comboWiki' )
    
    Bug: T430821
    Change-Id: I6820b8a7326c9eb672deaf5fb33d875fda9413bd

--- special/ComboWiki.php
+++ special/ComboWiki.php
@@ -1,20 +1,25 @@
 <?php
 
 use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\DonationInterface\Special\GatewayRouter;
 use MediaWiki\Html\Html;
 use MediaWiki\SpecialPage\UnlistedSpecialPage;
+use Psr\Log\LoggerInterface;
 
 /**
  * ComboWiki: the single-page VueJS donation flow.
  *
- * Skeleton special page that boots the ComboWiki Vue application. Modelled on
- * DonorPortal: it loads the Vue + styles ResourceLoader modules, sets up the
+ * Special page that sets up the ComboWiki Vue application.
+ * It loads the Vue + styles ResourceLoader modules, sets up the
  * viewport, and exposes server-side configuration to the client through the
- * MakeGlobalVariablesScript hook.
+ * MakeGlobalVariablesScript hook using setClientVariables().
  */
 class ComboWiki extends UnlistedSpecialPage {
 
+	private LoggerInterface $logger;
+
 	public function __construct() {
+		$this->logger = DonationLoggerFactory::getLoggerForType( 'GatewayAdapter', 'ComboWiki' );
 		parent::__construct( 'ComboWiki' );
 	}
 
@@ -41,10 +46,13 @@ class ComboWiki extends UnlistedSpecialPage {
 	 * @return void
 	 */
 	public function setClientVariables( array &$vars ): void {
-		// TODO: populate with the data the form needs (amounts, currency,
-		// country, gateway config, tracking params, etc.) as the build grows.
+		$params = $this->getRoutingParams();
+		$selectedGateway = $this->chooseGateway( $params );
+
 		$vars['comboWiki'] = [
 			'language' => $this->getLanguage()->getCode(),
+			'params' => $params,
+			'gateway' => $selectedGateway
 		];
 	}
 
@@ -86,5 +94,60 @@ class ComboWiki extends UnlistedSpecialPage {
 			'rel' => 'dns-prefetch',
 			'href' => 'https://upload.wikimedia.org'
 		] );
+	}
+
+	private function getRoutingParams(): array {
+		$country = $this->getRequest()->getVal( 'country' );
+		if ( !CountryValidation::isValidIsoCode( $country ) ) {
+			$country = CountryValidation::lookUpCountry( $this->getRequest()->getIP() );
+		}
+
+		$recurringRawValue = $this->getRequest()->getVal( 'recurring' );
+		if ( $recurringRawValue === 'false' ) {
+			$recurring = 0;
+		} elseif ( $recurringRawValue === 'true' ) {
+			$recurring = 1;
+		} else {
+			$recurring = (int)$recurringRawValue;
+		}
+
+		return [
+			'country' => $country,
+			'currency' => $this->getRequest()->getVal( 'currency' ),
+			'payment_method' => $this->getRequest()->getVal( 'payment_method' ),
+			'payment_submethod' => $this->getRequest()->getVal( 'payment_submethod' ),
+			'recurring' => $recurring,
+			'gateway' => $this->getRequest()->getVal( 'gateway' ),
+			'variant' => $this->getRequest()->getVal( 'variant' ),
+		];
+	}
+
+	private function chooseGateway( array $params ): ?string {
+		$supportedGateways = GatewayRouter::getSupportedGateways(
+			$params['country'],
+			$params['currency'],
+			$params['payment_method'],
+			$params['payment_submethod'],
+			(bool)$params['recurring'],
+			$params['variant'],
+			$this->getConfig()
+		);
+
+		if ( count( $supportedGateways ) === 0 ) {
+			$this->logger->error( 'No supported gateway for parameters: ' . print_r( $params, true ) );
+			return null;
+		}
+
+		if ( $params['gateway'] && in_array( $params['gateway'], $supportedGateways, true ) ) {
+			return $params['gateway'];
+		}
+
+		if ( count( $supportedGateways ) === 1 ) {
+			return $supportedGateways[0];
+		}
+
+		return GatewayRouter::chooseGatewayByPriority(
+			$supportedGateways, $params, $this->getConfig(), $this->logger
+		);
 	}
 }
---
+++ tests/phpunit/ComboWikiTest.php
@@ -0,0 +1,98 @@
+<?php
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Request\FauxRequest;
+use MediaWiki\Title\Title;
+
+/**
+ * @group Fundraising
+ * @group DonationInterface
+ * @group ComboWiki
+ * @group Database
+ * @covers \ComboWiki
+ */
+class ComboWikiTest extends DonationInterfaceTestCase {
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->overrideConfigValues( [
+			'DlocalGatewayEnabled' => true,
+			'BraintreeGatewayEnabled' => true,
+			'PaypalExpressGatewayEnabled' => true,
+			'AdyenCheckoutGatewayEnabled' => true,
+			'AmazonGatewayEnabled' => true,
+			'GravyGatewayEnabled' => true,
+			'DonationInterfaceGatewayAdapters' => [
+				'ingenico' => 'IngenicoAdapter',
+				'amazon' => 'AmazonAdapter',
+				'adyen' => 'AdyenCheckoutAdapter',
+				'paypal_ec' => 'PaypalExpressAdapter',
+				'braintree' => 'BraintreeAdapter',
+				'dlocal' => 'DlocalAdapter',
+				'gravy' => 'GravyAdapter',
+			],
+			'DonationInterfaceGatewayPriorityRules' => [
+				[
+					'conditions' => [ 'payment_method' => 'cc' ],
+					'gateways' => [ 'gravy' ],
+				],
+				[
+					'conditions' => [ 'payment_method' => 'paypal' ],
+					'gateways' => [ 'paypal_ec' ],
+				],
+				[
+					'conditions' => [ 'payment_method' => 'venmo' ],
+					'gateways' => [ 'braintree' ],
+				],
+			],
+		] );
+	}
+
+	public function testChoosesGravyForCreditCard(): void {
+		$this->assertChosenGateway(
+			[
+				'payment_method' => 'cc',
+				'country' => 'US',
+				'currency' => 'USD',
+				'recurring' => '0',
+			],
+			'gravy'
+		);
+	}
+
+	public function testChoosesPaypalForPaypal(): void {
+		$this->assertChosenGateway(
+			[
+				'payment_method' => 'paypal',
+				'country' => 'US',
+				'currency' => 'USD',
+				'recurring' => '0',
+			],
+			'paypal_ec'
+		);
+	}
+
+	public function testChoosesBraintreeForVenmo(): void {
+		$this->assertChosenGateway(
+			[
+				'payment_method' => 'venmo',
+				'country' => 'US',
+				'currency' => 'USD',
+				'recurring' => '0',
+			],
+			'braintree'
+		);
+	}
+
+	private function assertChosenGateway( array $params, string $expectedGateway ): void {
+		$context = RequestContext::getMain();
+		$context->setRequest( new FauxRequest( $params, false ) );
+		$context->setTitle( Title::newFromText( 'Special:ComboWiki' ) );
+
+		$comboWiki = new ComboWiki();
+		$vars = [];
+		$comboWiki->setClientVariables( $vars );
+
+		$this->assertEquals( $expectedGateway, $vars['comboWiki']['gateway'] );
+	}
+}
