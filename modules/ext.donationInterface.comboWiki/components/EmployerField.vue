d7eea9b75792f1598ff04ad7ad4fd9f2d0324d58
wfan <wfan@wikimedia.org>
2026-08-07 17:54:53 -0700

    Add Employer field to all payment method
    show it as we did for opt in for now
    
    Bug: T433142
    Change-Id: I22e708e65add9c7f0b6b2ecca09dae7838c5d294

---
+++ modules/ext.donationInterface.comboWiki/components/EmployerField.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="combo-wiki__employer">
+	<cdx-field :is-required="false">
+	  <cdx-label input-id="combo-employer-name">
+		{{ employerLabel }}
+	  </cdx-label>
+	  <cdx-text-input
+		id="combo-employer-name"
+		:model-value="modelValue"
+		@update:model-value="updateEmployer">
+	  </cdx-text-input>
+	</cdx-field>
+  </div>
+</template>
+
+<script>
+const { defineComponent } = require( 'vue' );
+const { CdxField, CdxLabel, CdxTextInput } = require( '@wikimedia/codex' );
+
+module.exports = defineComponent( {
+	name: 'EmployerField',
+	components: {
+		'cdx-field': CdxField,
+		'cdx-label': CdxLabel,
+		'cdx-text-input': CdxTextInput
+	},
+	props: {
+		modelValue: {
+			type: String,
+			default: ''
+		}
+	},
+	emits: [ 'update:modelValue' ],
+	computed: {
+		employerLabel() {
+			return this.$i18n( 'donate_interface-donor-employer' ).text();
+		}
+	},
+	methods: {
+		updateEmployer( value ) {
+			const sanitized = ( value || '' ).trimStart();
+			this.$emit( 'update:modelValue', sanitized );
+		}
+	}
+} );
+</script>
